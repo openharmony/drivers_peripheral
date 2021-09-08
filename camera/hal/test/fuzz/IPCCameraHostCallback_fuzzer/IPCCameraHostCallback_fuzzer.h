06548dc37a4bb986b2b21b813e1f2040aeedfbf0
guan_renjie <guan_renjie@hoperun.com>
2021-09-09 01:05:22 +0800

    提交FUZZ & UT
    Signed-off-by: guan_renjie <guan_renjie@hoperun.com>

--- camera/hal/test/fuzz/IPCCameraHostCallback_fuzzer/IPCCameraHostCallback_fuzzer.h
+++ camera/hal/test/fuzz/IPCCameraHostCallback_fuzzer/IPCCameraHostCallback_fuzzer.h
@@ -3,7 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -11,27 +13,14 @@
  * limitations under the License.
  */
 
-#include "transform_node.h"
+#ifndef IPC_CAMERA_HOST_CALLBACK_FUZZER_H
+#define IPC_CAMERA_HOST_CALLBACK_FUZZER_H
+#include <cstdint>
+#include <unistd.h>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
 
-namespace OHOS::Camera {
-TransformNode::TransformNode(const std::string& name, const std::string& type)
-    :NodeBase(name, type, streamId)
-{
-    CAMERA_LOGV("%{public}s enter, type(%{public}s)\n", name_.c_str(), type_.c_str());
-}
-
-RetCode TransformNode::Start(const int32_t streamId)
-{
-    return RC_OK;
-}
-
-RetCode TransformNode::Stop(const int32_t streamId)
-{
-    return RC_OK;
-}
-
-void TransformNode::AllocateBuffers()
-{
-}
-REGISTERNODE(TransformNode, {"transform"})
-}// namespace OHOS::Camera
+#define FUZZ_PROJECT_NAME "IPCCameraHostCallback_fuzzer"
+#endif
