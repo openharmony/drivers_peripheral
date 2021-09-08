41f800f71b68e5f94540274a1d4f9615d8163104
jiachanglin <jiachanglin@huawei.com>
2021-09-08 03:16:40 +0000

    Description:hdi callback about cpp Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: jiachanglin <jiachanglin@huawei.com>

--- wlan/hdi_service/client/wlan_hal_proxy.cpp
+++ wlan/hdi_service/client/wlan_hal_proxy.cpp
@@ -291,7 +291,7 @@ int32_t WlanInterfaceProxy::resetDriver(const uint8_t chipId)
     return ret;
 }
 
-int32_t WlanInterfaceProxy::getAsscociatedStas(std::shared_ptr<WifiFeatureInfo> ifeature, 
+int32_t WlanInterfaceProxy::getAsscociatedStas(std::shared_ptr<WifiFeatureInfo> ifeature,
     std::shared_ptr<StaInfo> staInfo, uint32_t count, std::vector<uint32_t>& num)
 {
     MessageParcel data;
