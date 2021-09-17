4f0e679b9dcb801bb11d84c32053bdad76c6a17e
jiachanglin <jiachanglin@huawei.com>
2021-09-17 10:00:40 +0000

    Description:Clean up the codex Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: jiachanglin <jiachanglin@huawei.com>

--- wlan/test/unittest/common/wifi_hal_test.cpp
+++ wlan/test/unittest/common/wifi_hal_test.cpp
@@ -73,7 +73,7 @@ void WifiHalTest::TearDown()
     ASSERT_EQ(HDF_SUCCESS, ret);
 }
 
-static void ParseScanResult(WifiScanResult *scanResult)
+static void ParseScanResult(const WifiScanResult *scanResult)
 {
     printf("ParseScanResult: flags=%d, caps=%d, freq=%d, beaconInt=%d,\n",
         scanResult->flags, scanResult->caps, scanResult->freq, scanResult->beaconInt);
@@ -81,7 +81,7 @@ static void ParseScanResult(WifiScanResult *scanResult)
         scanResult->qual, scanResult->beaconIeLen, scanResult->level, scanResult->age, scanResult->ieLen);
 }
 
-static int32_t HalCallbackEventScanResult(uint32_t eventId, void *data, const char *ifName)
+static int32_t HalCallbackEventScanResult(uint32_t eventId, const void *data, const char *ifName)
 {
     printf("HalCallbackEventScanResult ifName = %s, eventId = %d\n", ifName, eventId);
     switch (eventId) {
