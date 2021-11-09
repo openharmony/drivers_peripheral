12ec57014fd5bf195c5c8187d53a00f958a911b2
zhao_haipeng <zhao_haipeng@hoperun.com>
2021-11-09 15:04:59 +0800

    modify copyright config file
    Signed-off-by: zhao_haipeng <zhao_haipeng@hoperun.com>

--- audio/supportlibs/tinyalsa_adapter/src/route_config.c
+++ audio/supportlibs/tinyalsa_adapter/src/route_config.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Rockchip Electronics Co. LTD
+ * Copyright (c) 2021 Rockchip Electronics Co., Ltd. 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
