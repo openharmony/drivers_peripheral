0dfe478bd6ef53d9c79540ab0fea359300ab7147
YOUR_NAME <fangdong7@huawei.com>
2021-12-01 07:21:04 +0000

    Description: wlan 5GHz adapter Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <fangdong7@huawei.com>

--- wlan/client/src/sbuf/sbuf_cmd_adapter.c
+++ wlan/client/src/sbuf/sbuf_cmd_adapter.c
@@ -1108,7 +1108,7 @@ int32_t WifiCmdGetHwFeature(const char *ifName, WifiHwFeatureData *hwFeatureData
         return RET_CODE_INVALID_PARAM;
     }
     data = HdfSBufObtainDefaultSize();
-    reply = HdfSBufObtain(sizeof(WifiHwFeatureData) + sizeof(hwFeatureData));
+    reply = HdfSBufObtain(sizeof(WifiHwFeatureData) + sizeof(uint64_t));
     if (data == NULL || reply == NULL) {
         ret = RET_CODE_FAILURE;
         goto RELEASE_DATA;
