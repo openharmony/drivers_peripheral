d11fee67a64c5673ee277c89b14b8c49a604fd34
YOUR_NAME <xieyingdong1@huawei.com>
2022-01-12 07:01:28 +0000

    fixed 7660bab from https://gitee.com/xie0812/drivers_peripheral/pulls/523 Display-Layer、Display-Gralloc、Display-Gfx针对轻量系统的增强参考实现
    Signed-off-by: YOUR_NAME <xieyingdong1@huawei.com>

--- display/hal/default/display_gralloc.c
+++ display/hal/default/display_gralloc.c
@@ -16,6 +16,7 @@
 #include "display_gralloc.h"
 #include <errno.h>
 #include <inttypes.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <sys/shm.h>
@@ -24,6 +25,7 @@
 #include "display_type.h"
 #include "disp_common.h"
 #include "hdf_log.h"
+#include "osal_mem.h"
 
 #define DEFAULT_READ_WRITE_PERMISSIONS   0666
 #define MAX_MALLOC_SIZE                  0x10000000L
@@ -32,11 +34,14 @@
 #define INVALID_SHMID -1
 #define BITS_PER_BYTE 8
 
-#define DIV_ROUND_UP(n, d) (((n) + (d)-1) / (d))
-#define ALIGN_UP(x, a) ((((x) + ((a)-1)) / (a)) * (a))
-#define HEIGHT_ALIGN 2U
-#define WIDTH_ALIGN 8U
-#define MAX_PLANES 3
+#define DIV_ROUND_UP(n, d)   (((n) + (d)-1) / (d))
+#define ALIGN_UP(x, a)       ((((x) + ((a)-1)) / (a)) * (a))
+#define HEIGHT_ALIGN         2U
+#define WIDTH_ALIGN          8U
+#define MAX_PLANES           3
+
+#undef  HDF_LOG_TAG
+#define HDF_LOG_TAG          display_gralloc_c
 
 typedef struct {
     BufferHandle hdl;
@@ -54,6 +59,14 @@ typedef struct {
     const PlaneLayoutInfo *planes;
 } FormatInfo;
 
+struct GrallocManager {
+    pthread_mutex_t mutex;
+    pthread_mutexattr_t mutexattr;
+    int32_t count;
+};
+
+static struct GrallocManager g_grallocManager;
+
 static const PlaneLayoutInfo g_yuv420SPLayout = {
     .numPlanes = 2,
     .radio = { 4, 2 },
@@ -63,6 +76,27 @@ static const PlaneLayoutInfo g_yuv420PLayout = {
     .numPlanes = 3,
     .radio = { 4, 1, 1 },
 };
+
+static void GetGrallocMgr(void)
+{
+    g_grallocManager.count++;
+}
+
+static int32_t PutGrallocMgr(void)
+{
+    g_grallocManager.count--;
+    return g_grallocManager.count;
+}
+
+static void LockGrallocMgr(void)
+{
+    pthread_mutex_lock(&g_grallocManager.mutex);
+}
+
+static void UnlockGrallocMgr(void)
+{
+    pthread_mutex_unlock(&g_grallocManager.mutex);
+}
 
 static const FormatInfo *GetFormatInfo(uint32_t format)
 {
@@ -156,6 +190,13 @@ static int32_t AllocShm(BufferHandle *buffer)
     buffer->key = key;
     ((PriBufferHandle*)buffer)->shmid = shmid;
     key++;
+    if (memset_s(pBase, buffer->size, 0x0, buffer->size) != EOK) {
+        HDF_LOGE("memset_s failure");
+        if (shmctl(shmid, IPC_RMID, 0) == -1) {
+            HDF_LOGE("%s: Fail to free shmid, errno = %d", __func__, errno);
+        }
+        return DISPLAY_FAILURE;
+    }
     if (key >= SHM_MAX_KEY) {
         key = SHM_START_KEY;
     }
@@ -165,7 +206,7 @@ static int32_t AllocShm(BufferHandle *buffer)
 static int32_t AllocMem(const AllocInfo* info, BufferHandle **buffer)
 {
     int32_t ret;
-    BufferHandle *bufferHdl = NULL;
+
     DISPLAY_CHK_RETURN((buffer == NULL), DISPLAY_NULL_PTR, HDF_LOGE("%s: in buffer is null", __func__));
     DISPLAY_CHK_RETURN((info == NULL), DISPLAY_NULL_PTR, HDF_LOGE("%s: in info is null", __func__));
     PriBufferHandle* priBuffer = calloc(1, sizeof(PriBufferHandle));
@@ -175,10 +216,10 @@ static int32_t AllocMem(const AllocInfo* info, BufferHandle **buffer)
     DISPLAY_CHK_RETURN((ret != DISPLAY_SUCCESS), DISPLAY_FAILURE, HDF_LOGE("%s: can not init buffe handle",
         __func__); goto OUT);
 
-    bufferHdl = &priBuffer->hdl;
+    BufferHandle *bufferHdl = &priBuffer->hdl;
     DISPLAY_CHK_RETURN(((bufferHdl->size > MAX_MALLOC_SIZE) || (bufferHdl->size == 0)),
-        DISPLAY_FAILURE, HDF_LOGE("%s: size is invalid %d ", __func__, bufferHdl->size); goto out);
-
+        DISPLAY_FAILURE, HDF_LOGE("%s: size is invalid %d ", __func__, bufferHdl->size); goto OUT);
+    LockGrallocMgr();
     if (bufferHdl->usage == HBM_USE_MEM_SHARE) {
         ret = AllocShm(bufferHdl);
     } else {
@@ -192,6 +233,7 @@ OUT:
         bufferHdl = NULL;
     }
     *buffer = bufferHdl;
+    UnlockGrallocMgr();
     return ret;
 }
 
@@ -214,23 +256,22 @@ static void FreeMem(BufferHandle *buffer)
         return;
     }
 
-    if (buffer->usage == HBM_USE_MEM_SHARE) {
-        FreeShm(buffer);
-        return;
-    } else {
-        HDF_LOGE("%s: not support memory usage: 0x%" PRIx64 "", __func__, buffer->usage);
+    LockGrallocMgr();
+    switch (buffer->usage) {
+        case HBM_USE_MEM_SHARE:
+            FreeShm(buffer);
+            break;
+        default:
+            HDF_LOGE("%s: not support memory usage: 0x%" PRIx64 "", __func__, buffer->usage);
     }
+    UnlockGrallocMgr();
 }
 
-static void *Mmap(BufferHandle *buffer)
+static void *MmapShm(BufferHandle *buffer)
 {
-    CHECK_NULLPOINTER_RETURN_VALUE(buffer, NULL);
-    if ((buffer->size > MAX_MALLOC_SIZE) || (buffer->size == 0)) {
-        HDF_LOGE("%s: size is invalid, buffer->size = %d", __func__, buffer->size);
-        return NULL;
-    }
+    int32_t shmid;
 
-    int32_t shmid = shmget(buffer->key, buffer->size, IPC_EXCL | DEFAULT_READ_WRITE_PERMISSIONS);
+    shmid = shmget(buffer->key, buffer->size, IPC_EXCL | DEFAULT_READ_WRITE_PERMISSIONS);
     if (shmid < 0) {
         HDF_LOGE("%s: Fail to mmap the shared memory, errno = %d", __func__, errno);
         return NULL;
@@ -245,15 +286,31 @@ static void *Mmap(BufferHandle *buffer)
     return pBase;
 }
 
-static int32_t Unmap(BufferHandle *buffer)
+static void *Mmap(BufferHandle *buffer)
 {
-    CHECK_NULLPOINTER_RETURN_VALUE(buffer, DISPLAY_NULL_PTR);
-    CHECK_NULLPOINTER_RETURN_VALUE(buffer->virAddr, DISPLAY_NULL_PTR);
+    void *temp = NULL;
+
+    CHECK_NULLPOINTER_RETURN_VALUE(buffer, NULL);
     if ((buffer->size > MAX_MALLOC_SIZE) || (buffer->size == 0)) {
         HDF_LOGE("%s: size is invalid, buffer->size = %d", __func__, buffer->size);
-        return DISPLAY_FAILURE;
+        return NULL;
     }
 
+    LockGrallocMgr();
+    switch (buffer->usage) {
+        case HBM_USE_MEM_SHARE:
+            temp = MmapShm(buffer);
+            break;
+        default:
+            HDF_LOGE("%s: not support memory usage: 0x%" PRIx64 "", __func__, buffer->usage);
+            break;
+    }
+    UnlockGrallocMgr();
+    return temp;
+}
+
+static int32_t UnmapShm(BufferHandle *buffer)
+{
     if (shmdt(buffer->virAddr) == -1) {
         HDF_LOGE("%s: Fail to unmap shared memory errno =  %d", __func__, errno);
         return DISPLAY_FAILURE;
@@ -265,23 +322,54 @@ static int32_t Unmap(BufferHandle *buffer)
     return DISPLAY_SUCCESS;
 }
 
+static int32_t Unmap(BufferHandle *buffer)
+{
+    int32_t ret;
+
+    CHECK_NULLPOINTER_RETURN_VALUE(buffer, DISPLAY_NULL_PTR);
+    CHECK_NULLPOINTER_RETURN_VALUE(buffer->virAddr, DISPLAY_NULL_PTR);
+    if ((buffer->size > MAX_MALLOC_SIZE) || (buffer->size == 0)) {
+        HDF_LOGE("%s: size is invalid, buffer->size = %d", __func__, buffer->size);
+        return DISPLAY_FAILURE;
+    }
+    LockGrallocMgr();
+    switch (buffer->usage) {
+        case  HBM_USE_MEM_SHARE:
+            ret = UnmapShm(buffer);
+            break;
+        default:
+            HDF_LOGE("%s: not support memory usage: 0x%" PRIx64 "", __func__, buffer->usage);
+            ret = DISPLAY_FAILURE;
+            break;
+    }
+    UnlockGrallocMgr();
+    return ret;
+}
+
 int32_t GrallocInitialize(GrallocFuncs **funcs)
 {
+    static GrallocFuncs *gFuncs = NULL;
+
     if (funcs == NULL) {
         HDF_LOGE("%s: funcs is null", __func__);
         return DISPLAY_NULL_PTR;
     }
-    GrallocFuncs *gFuncs = (GrallocFuncs *)malloc(sizeof(GrallocFuncs));
     if (gFuncs == NULL) {
-        HDF_LOGE("%s: gFuncs is null", __func__);
-        return DISPLAY_NULL_PTR;
+        gFuncs = (GrallocFuncs *)OsalMemCalloc(sizeof(GrallocFuncs));
+        if (gFuncs == NULL) {
+            HDF_LOGE("%s: gFuncs is null", __func__);
+            return DISPLAY_NULL_PTR;
+        }
+        pthread_mutexattr_init(&g_grallocManager.mutexattr);
+        pthread_mutexattr_setpshared(&g_grallocManager.mutexattr, PTHREAD_PROCESS_SHARED);
+        pthread_mutex_init(&g_grallocManager.mutex, &g_grallocManager.mutexattr);
+        gFuncs->AllocMem = AllocMem;
+        gFuncs->FreeMem = FreeMem;
+        gFuncs->Mmap = Mmap;
+        gFuncs->Unmap = Unmap;
     }
-    (void)memset_s(gFuncs, sizeof(GrallocFuncs), 0, sizeof(GrallocFuncs));
-    gFuncs->AllocMem = AllocMem;
-    gFuncs->FreeMem = FreeMem;
-    gFuncs->Mmap = Mmap;
-    gFuncs->Unmap = Unmap;
     *funcs = gFuncs;
+    GetGrallocMgr();
     HDF_LOGI("%s: gralloc initialize success", __func__);
     return DISPLAY_SUCCESS;
 }
@@ -292,7 +380,11 @@ int32_t GrallocUninitialize(GrallocFuncs *funcs)
         HDF_LOGE("%s: funcs is null", __func__);
         return DISPLAY_NULL_PTR;
     }
-    free(funcs);
+    if (PutGrallocMgr() == 0) {
+        pthread_mutexattr_destroy(&g_grallocManager.mutexattr);
+        pthread_mutex_destroy(&g_grallocManager.mutex);
+        OsalMemFree(funcs);
+    }
     HDF_LOGI("%s: gralloc uninitialize success", __func__);
     return DISPLAY_SUCCESS;
 }
