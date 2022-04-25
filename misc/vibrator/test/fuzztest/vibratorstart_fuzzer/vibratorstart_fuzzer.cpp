c7a1ee1071332c09da0079f988ea9334bd46990f
sunxuejiao <sunxuejiao5@huawei.com>
2022-04-25 09:32:06 +0800

    fix:add vibrator/light fuzztest
    Signed-off-by: sunxuejiao <sunxuejiao5@huawei.com>

---
+++ misc/vibrator/test/fuzztest/vibratorstart_fuzzer/vibratorstart_fuzzer.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vibratorstart_fuzzer.h"
+#include "hdf_base.h"
+#include "v1_0/vibrator_interface_proxy.h"
+
+using namespace OHOS::HDI::Vibrator::V1_0;
+
+namespace OHOS {
+    bool VibratorStartFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        sptr<IVibratorInterface> g_vibratorInterface = IVibratorInterface::Get();
+        if (!g_vibratorInterface->Start(reinterpret_cast<const std::string &>(data))) {
+            result = true;
+        }
+        return result;
+    }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    OHOS::VibratorStartFuzzTest(data, size);
+    return 0;
+}
