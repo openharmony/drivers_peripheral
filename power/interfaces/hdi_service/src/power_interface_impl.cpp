8c98b9d58d9ab4e156f5f71ffe1c16b9016092f0
toutes <zhaoruiyuan2@huawei.com>
2022-04-25 02:45:38 -0700

    fix: Overload the loadstrngfd function to prevent the kernel scheduling from returning
    Signed-off-by: toutes <zhaoruiyuan2@huawei.com>
    Change-Id: I649812bc2e66dd3fd98c679957f95ef1d4e59fb5

--- power/interfaces/hdi_service/src/power_interface_impl.cpp
+++ power/interfaces/hdi_service/src/power_interface_impl.cpp
@@ -19,6 +19,9 @@
 #include <hdf_base.h>
 #include <file_ex.h>
 #include <sys/eventfd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
@@ -27,6 +30,7 @@
 #include "errors.h"
 #include "hdf_sbuf.h"
 #include "pubdef.h"
+#include "securec.h"
 #include "utils/hdf_log.h"
 #include "hdf_device_desc.h"
 #include "hdf_remote_service.h"
@@ -52,6 +56,7 @@ static sptr<IPowerHdiCallback> callback_;
 static UniqueFd wakeupCountFd;
 static void AutoSuspendLoop();
 static int32_t DoSuspend();
+static bool LoadStringFd(int32_t fd, std::string& content);
 static std::string ReadWakeCount();
 static bool WriteWakeCount(const std::string& count);
 static void NotifyCallback(int code);
@@ -182,13 +187,38 @@ int32_t PowerInterfaceImpl::SuspendUnblock(const std::string& name)
     return HDF_SUCCESS;
 }
 
+bool LoadStringFd(int32_t fd, std::string& content)
+{
+    if (fd <= 0) {
+        HDF_LOGE("invalid fd: %{public}d", fd);
+        return false;
+    }
+ 
+    struct stat statBuf;
+    (void)memset_s(&statBuf, sizeof(struct stat), 0, sizeof(struct stat));
+    if (fstat(fd, &statBuf) != 0) {
+        HDF_LOGE("Failed to get the file size");
+        return false;
+    }
+    const int32_t fileLength = static_cast<uint32_t>(statBuf.st_size);
+
+    const int32_t len = TEMP_FAILURE_RETRY(read(fd, content.data(), static_cast<size_t>(fileLength)));
+    if (len < 0) {
+        HDF_LOGE("the length read from file is not equal to fileLength!len: %{public}d,fileLen: %{public}d",
+            len, fileLength);
+        return false;
+    }
+
+    return true;
+}
+
 std::string ReadWakeCount()
 {
     if (wakeupCountFd < 0) {
         wakeupCountFd = UniqueFd(TEMP_FAILURE_RETRY(open(WAKEUP_COUNT_PATH, O_RDWR | O_CLOEXEC)));
     }
     std::string wakeupCount;
-    LoadStringFromFd(wakeupCountFd, wakeupCount);
+    LoadStringFd(wakeupCountFd, wakeupCount);
 
     return wakeupCount;
 }
