d787bb7cce73809f5af6e52410df3a4587e8c6c0
sunxuejiao <sunxuejiao5@huawei.com>
2022-05-05 14:40:15 +0800

    test:add display fuzztest
    Signed-off-by: sunxuejiao <sunxuejiao5@huawei.com>

---
+++ display/test/fuzztest/displayissupportedalloc_fuzzer/displayissupportedalloc_fuzzer.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "displayissupportedalloc_fuzzer.h"
+#include "buffer_handle.h"
+#include "display_gralloc_client.h"
+
+using namespace OHOS::HDI::Display::V1_0;
+
+namespace OHOS {
+    bool DisplayIssupportedAllocFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        std::vector<bool> supporteds;
+        int32_t ret;
+
+        ret = DisplayGrallocClient::Get()->IsSupportedAlloc(
+            reinterpret_cast<std::vector<VerifyAllocInfo> &>(data), supporteds);
+        if (!ret) {
+            result = true;
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DisplayIssupportedAllocFuzzTest(data, size);
+    return 0;
+}
+
---
+++ display/test/fuzztest/displaymmap_fuzzer/displaymmap_fuzzer.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "displaymmap_fuzzer.h"
+#include "buffer_handle.h"
+#include "display_gralloc_client.h"
+
+using namespace OHOS::HDI::Display::V1_0;
+
+namespace OHOS {
+    bool DisplayMapFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        int32_t ret;
+        BufferHandle handle;
+        handle.height = static_cast<int32_t>(*data);
+        handle.width = static_cast<int32_t>(*data);
+        
+        if (!DisplayGrallocClient::Get()->Mmap(handle)) {
+            result = true;
+        }
+
+        ret = DisplayGrallocClient::Get()->Unmap(handle);
+        if (!ret) {
+            result = true;
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DisplayMapFuzzTest(data, size);
+    return 0;
+}
+
