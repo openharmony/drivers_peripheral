d01f6d0058c850ed2cc80f07ce448ae4efc0867b
muxiaoximuxiaoxi <sunhaohao5@huawei.com>
2022-05-25 20:23:46 -0700

    optimize display log
    Signed-off-by: muxiaoximuxiaoxi <sunhaohao5@huawei.com>

--- display/hdi_service/gralloc/client/display_gralloc_client.cpp
+++ display/hdi_service/gralloc/client/display_gralloc_client.cpp
@@ -45,7 +45,6 @@ DisplayGrallocClient::DisplayGrallocClient()
     if (allocatorProxy_ == nullptr) {
         return;
     }
-    HDF_LOGI("%{public}s instance ok", __func__);
 }
 
 int32_t DisplayGrallocClient::AllocMem(const AllocInfo &info, BufferHandle *&handle) const
