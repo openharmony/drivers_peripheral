1b2d2c0ce085272ffb93dcac567740398581e895
“wangyougang” <wangyougang2@huawei.com>
2022-05-26 04:13:08 +0000

    fix:modify codex of netlink
    Signed-off-by: “wangyougang” <wangyougang2@huawei.com>

--- wlan/client/src/netlink/netlink_cmd_adapter.c
+++ wlan/client/src/netlink/netlink_cmd_adapter.c
@@ -885,6 +885,7 @@ int32_t GetValidFreqByBand(const char *ifName, int32_t band, struct FreqInfoResu
     ret = memset_s(result->freqs, size * sizeof(int32_t), 0, sizeof(result->freqs));
     if (ret != EOK) {
         HILOG_ERROR(LOG_DOMAIN, "%s: memset_s result->freqs  failed", __FUNCTION__);
+        nlmsg_free(msg);
         return RET_CODE_FAILURE;
     }
     result->nums = 0;
@@ -1225,6 +1226,7 @@ int32_t WifiCmdScan(const char *ifName, WifiScan *scan)
         goto err;
     }
 
+    nlmsg_free(msg);
     return RET_CODE_SUCCESS;
 err:
     nlmsg_free(msg);
