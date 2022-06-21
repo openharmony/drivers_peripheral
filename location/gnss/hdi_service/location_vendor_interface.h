c5314b932b89876bf6c2c1d8b83a2af3301496cf
liu-binjun <liubinjun@huawei.com>
2022-06-21 20:36:22 +0800

    feat: add location HDI modules
    Signed-off-by: liu-binjun <liubinjun@huawei.com>

---
+++ location/gnss/hdi_service/location_vendor_interface.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_HDI_LOCATION_LOCATION_VENDOR_INTERFACE_H
+#define OHOS_HDI_LOCATION_LOCATION_VENDOR_INTERFACE_H
+
+#include <functional>
+#include <vector>
+#include "singleton.h"
+#include "location_vendor_lib.h"
+
+namespace OHOS {
+namespace HDI {
+namespace Location {
+class LocationVendorInterface {
+public:
+    static void Init();
+    static void CleanUp();
+    static const GnssVendorInterface *GetVendorInterface();
+    static const void *GetModuleInterface(int moduleId);
+};
+} // Location
+} // HDI
+} // OHOS
+#endif /* OHOS_HDI_LOCATION_LOCATION_VENDOR_INTERFACE_H */
