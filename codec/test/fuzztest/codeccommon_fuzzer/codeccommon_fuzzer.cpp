080be0e0f3a47a56829cdb907ea277c0bfcc310b
lwx1140380 <lihuaicheng3@huawei.com>
2022-06-21 16:57:20 +0800

    codec fuzz test case function encapsulation with interface parameter modification
    Signed-off-by: lwx1140380 <lihuaicheng3@huawei.com>

---
+++ codec/test/fuzztest/codeccommon_fuzzer/codeccommon_fuzzer.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "codeccommon_fuzzer.h"
+
+namespace OHOS {
+namespace Codec {
+    static const int32_t DATA_BUFFERID = 10;
+    static const int32_t DATA_SIZE = 20;
+    static const int32_t DATA_VERSION_NVERSION = 30;
+    static const int32_t DATA_BUFFERTYPE = 40;
+    static const int32_t DATA_BUFFERLEN = 50;
+    static const int32_t DATA_ALLOCLEN = 60;
+    static const int32_t DATA_FILLEDLEN = 70;
+    static const int32_t DATA_OFFSET = 80;
+    static const int32_t DATA_FENCEFD = 90;
+    static const int32_t DATA_TYPE = 100;
+    static const int32_t DATA_PTS = 200;
+    static const int32_t DATA_FLAG = 300;
+    static const int32_t testingAppData = 33;
+
+    CodecComponentManager *manager = nullptr;
+    CodecComponentType *component = nullptr;
+    CodecCallbackType *callback = nullptr;
+    uint32_t componentId = 0;
+    static int32_t appData = testingAppData;
+
+    void FillDataOmxCodecBuffer(struct OmxCodecBuffer *dataFuzz)
+    {
+        dataFuzz->bufferId = DATA_BUFFERID;
+        dataFuzz->size = DATA_SIZE;
+        dataFuzz->version.nVersion = DATA_VERSION_NVERSION;
+        dataFuzz->bufferType = (enum CodecBufferType)DATA_BUFFERTYPE;
+        dataFuzz->buffer = (uint8_t*)OsalMemAlloc(DATA_BUFFERLEN);
+        dataFuzz->bufferLen = DATA_BUFFERLEN;
+        dataFuzz->allocLen = DATA_ALLOCLEN;
+        dataFuzz->filledLen = DATA_FILLEDLEN;
+        dataFuzz->offset = DATA_OFFSET;
+        dataFuzz->fenceFd = DATA_FENCEFD;
+        dataFuzz->type = (enum ShareMemTypes)DATA_TYPE;
+        dataFuzz->pts = DATA_PTS;
+        dataFuzz->flag = DATA_FLAG;
+    }
+
+    bool Preconditions()
+    {
+        manager = GetCodecComponentManager();
+        callback = CodecCallbackTypeStubGetInstance();
+        if (manager == nullptr) {
+            HDF_LOGE("%{public}s: GetCodecComponentManager failed\n", __func__);
+            return false;
+        }
+
+        int32_t ret = manager->CreateComponent(&component, &componentId, (char*)"compName", appData, callback);
+        if (ret != HDF_SUCCESS) {
+            HDF_LOGE("%{public}s: CreateComponent failed\n", __func__);
+            return false;
+        }
+
+        OMX_STATETYPE state;
+        ret = component->GetState(component, &state);
+        if (ret != HDF_SUCCESS) {
+            HDF_LOGE("%{public}s: GetState Component faild\n", __func__);
+            return false;
+        }
+
+        return true;
+    }
+
+    bool Destory()
+    {
+        int32_t ret = manager->DestoryComponent(componentId);
+        if (ret != HDF_SUCCESS) {
+            HDF_LOGE("%{public}s: DestoryComponent failed\n", __func__);
+            return false;
+        }
+        CodecComponentTypeRelease(component);
+        CodecComponentManagerRelease();
+        return true;
+    }
+} // namespace codec
+} // namespace OHOS
--- codec/test/fuzztest/codeccomponentroleenum_fuzzer/codeccomponentroleenum_fuzzer.cpp
+++ codec/test/fuzztest/codeccomponentroleenum_fuzzer/codeccomponentroleenum_fuzzer.cpp
@@ -14,50 +14,47 @@
  */
 
 #include "codeccomponentroleenum_fuzzer.h"
-#include "codec_callback_type_stub.h"
-#include "codec_component_type.h"
-#include "codec_component_manager.h"
+#include "codeccommon_fuzzer.h"
 
-#include <osal_mem.h>
-#include <hdf_log.h>
+#include <securec.h>
+
+struct AllParameters {
+    uint8_t *role;
+    uint32_t roleLen;
+    uint32_t index;
+};
 
 namespace OHOS {
 namespace Codec {
     bool CodecComponentRoleEnum(const uint8_t* data, size_t size)
     {
+        struct AllParameters params;
+        if (data == nullptr) {
+            return false;
+        }
+
+        if (memcpy_s((void *)&params, sizeof(params), data, sizeof(params)) != 0) {
+            return false;
+        }
+
         bool result = false;
-        const int32_t testingAppData = 33;
-        struct CodecComponentManager *manager = nullptr;
-        struct CodecComponentType *component = nullptr;
-        int32_t appData = testingAppData;
-        CodecCallbackType* callback = CodecCallbackTypeStubGetInstance();
-        uint32_t componentId = 0;
-        manager = GetCodecComponentManager();
-        if (manager == nullptr) {
-            HDF_LOGE("%{public}s: GetCodecComponentManager failed\n", __func__);
+        result = Preconditions();
+        if (!result) {
+            HDF_LOGE("%{public}s: Preconditions failed\n", __func__);
             return false;
         }
 
-        int32_t ret = manager->CreateComponent(&component, &componentId, (char*)"compName", appData, callback);
-        if (ret != HDF_SUCCESS) {
-            HDF_LOGE("%{public}s: CreateComponent failed\n", __func__);
-            return false;
-        }
-
-        ret = component->ComponentRoleEnum(component, const_cast<uint8_t *>(data),
-            *(uint32_t *)data, *(uint32_t *)data);
+        int32_t ret = component->ComponentRoleEnum(component, params.role, params.roleLen, params.index);
         if (ret == HDF_SUCCESS) {
             HDF_LOGI("%{public}s: ComponentRoleEnum succeed\n", __func__);
             result = true;
         }
 
-        ret = manager->DestoryComponent(componentId);
-        if (ret != HDF_SUCCESS) {
-            HDF_LOGE("%{public}s: DestoryComponent failed\n", __func__);
+        result = Destory();
+        if (!result) {
+            HDF_LOGE("%{public}s: Destory failed\n", __func__);
             return false;
         }
-        CodecComponentTypeRelease(component);
-        CodecComponentManagerRelease();
 
         return result;
     }
