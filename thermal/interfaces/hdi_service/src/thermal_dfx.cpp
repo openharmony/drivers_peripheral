610f3b7f93b239081101acac078baf9296c9f26c
zhengpengyue <zhengpengyue1@huawei.com>
2022-08-25 16:06:46 +0800

    fix: Modify powermgr coding specification
    Signed-off-by: zhengpengyue <zhengpengyue1@huawei.com>
    Change-Id: Ide91fabe0460316d2cf265f041d54a3c862b1237

--- thermal/interfaces/hdi_service/src/thermal_dfx.cpp
+++ thermal/interfaces/hdi_service/src/thermal_dfx.cpp
@@ -67,6 +67,10 @@ static std::string GetCurrentTime(const int32_t format)
     }
 
     pTime = localtime(&t);
+    if (pTime == nullptr) {
+        THERMAL_HILOGW(COMP_HDI, "pTime Get localtime failed");
+        return "";
+    }
     if (format == TIME_FORMAT_1) {
         if (strftime(strTime, sizeof(strTime), "%Y%m%d-%H%M%S", pTime) == 0U) {
             THERMAL_HILOGW(COMP_HDI, "call strfime failed");
@@ -300,7 +304,7 @@ void ThermalDfx::ProcessLogInfo(std::string& logFile, bool isEmpty)
 {
     std::string width = OHOS::system::GetParameter("persist.thermal.log.width", "20");
     std::string interval = OHOS::system::GetParameter("persist.thermal.log.interval", "5000");
-    uint32_t paramWidth = std::stoi(width.c_str());
+    uint32_t paramWidth = static_cast<uint32_t>(std::stoi(width.c_str()));
 
     std::string currentTime = GetCurrentTime(TIME_FORMAT_2);
     std::ofstream wStream(logFile, std::ios::app);
