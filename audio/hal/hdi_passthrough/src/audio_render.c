12f5cbc4f15ad4e16392725492c69ab468b44c4d
janiervan <fanjingya1@huawei.com>
2022-09-07 17:43:12 +0800

    fix：Delete redundant dependency of AUDIO module
    Signed-off-by: janiervan <fanjingya1@huawei.com>

--- audio/hal/hdi_passthrough/src/audio_render.c
+++ audio/hal/hdi_passthrough/src/audio_render.c
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//aaaaaaaaa
 #include "audio_render.h"
 #include "osal_mem.h"
 #include "audio_adapter_info_common.h"
