c88b42d0c3c8509500daa9947b5bdbea7cc44a79
wangchaole <wangchaole1@huawei.com>
2022-10-18 14:22:17 +0800

    fead: add fuzz test to dcamera
    Signed-off-by: wangchaole <wangchaole1@huawei.com>

---
+++ distributed_camera/hdi_service/test/fuzztest/dcameragetcameraability_fuzzer/dcameragetcameraability_fuzzer.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dcameragetcameraability_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "dcamera_host.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+void DcameraGetCameraAbilityFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size < sizeof(uint8_t))) {
+        return;
+    }
+    std::string cameraId(reinterpret_cast<const char*>(data), size);
+    std::vector<uint8_t> cameraAbility;
+    cameraAbility.push_back(* data);
+    DCameraHost::GetInstance()->GetCameraAbility(cameraId, cameraAbility);
+}
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DistributedHardware::DcameraGetCameraAbilityFuzzTest(data, size);
+    return 0;
+}
+
