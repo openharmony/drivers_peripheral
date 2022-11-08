fd5df5a020e9b0cb678bf3000e253d9ae0e1340f
wang.xin_wang <wang.xin_wang@neusoft.com>
2022-11-08 16:37:26 +0800

    Add DideRecipient test cases
    Signed-off-by: wang.xin_wang <wang.xin_wang@neusoft.com>

---
+++ display/standard/buffer/test/unittest/death_test.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DISPLAY_BUFFER_UT_H
+#define DISPLAY_BUFFER_UT_H
+#include "gtest/gtest.h"
+#include "v1_0/iallocator.h"
+#include "v1_0/imapper.h"
+#include "v1_0/display_buffer_type.h"
+#include "v1_0/include/idisplay_buffer.h"
+
+using OHOS::HDI::Display::Buffer::V1_0::IAllocator;
+using OHOS::HDI::Display::Buffer::V1_0::IMapper;
+using OHOS::HDI::Display::Buffer::V1_0::AllocInfo;
+using OHOS::HDI::Display::Buffer::V1_0::IDisplayBuffer;
+
+namespace {
+class DeathTest : public::testing::Test {
+protected:
+    virtual void SetUp();
+    virtual void TearDown();
+public:
+    IDisplayBuffer* displayBuffer_{ nullptr };
+};
+
+class BufferDideRecipient : public OHOS::IRemoteObject::DeathRecipient {
+public:
+    BufferDideRecipient() = default;
+    virtual ~BufferDideRecipient() = default;
+    void OnRemoteDied(const OHOS::wptr<OHOS::IRemoteObject>& remote) override;
+};
+}
+#endif // DISPLAY_BUFFER_UT_H
