536602fc6149c4cbf8c7c0aed4597fbc9fa8e4d6
vb6174 <zhangyunhu@huawei.com>
2022-12-01 17:59:25 +0800

    fix: audio alsa adapter build fail
    Signed-off-by: vb6174 <zhangyunhu@huawei.com>

--- audio/supportlibs/alsa_adapter/src/alsa_lib_render.c
+++ audio/supportlibs/alsa_adapter/src/alsa_lib_render.c
@@ -188,8 +188,6 @@ int32_t AudioCtlRenderGetVolume(const struct DevHandle *handle, int cmdId, struc
 int32_t AudioCtlRenderSetPauseStu(
     const struct DevHandle *handle, int cmdId, const struct AudioHwRenderParam *handleData)
 {
-    struct AudioCardInfo *cardIns = NULL;
-
     (void)cmdId;
     if (handle == NULL || handleData == NULL) {
         AUDIO_FUNC_LOGE("Parameter error!");
