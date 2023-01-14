0f2823abd4aeef271a0e6f1bafb7e56fca221b2a
zhusiyuan <zhusiyuan2@huawei.com>
2023-01-14 03:04:02 +0000

    Fix: Clean display unittest alarm
    Signed-off-by: zhusiyuan <zhusiyuan2@huawei.com>

--- display/test/unittest/standard/display_gralloc/display_gralloc_test.cpp
+++ display/test/unittest/standard/display_gralloc/display_gralloc_test.cpp
@@ -42,7 +42,7 @@ const AllocTestPrms GRALLOC_TEST_SETS[] = {
             .height = 1920,
             .usage = HBM_USE_MEM_DMA | HBM_USE_CPU_READ | HBM_USE_CPU_WRITE,
             .format = PIXEL_FMT_RGBX_8888
-            },
+        },
         .expectStride = 1088 * 4,
         .expectSize = 1920 * 1088 * 4
     },
@@ -64,7 +64,7 @@ const AllocTestPrms GRALLOC_TEST_SETS[] = {
             .height = 1920,
             .usage = HBM_USE_MEM_DMA | HBM_USE_CPU_READ | HBM_USE_CPU_WRITE,
             .format = PIXEL_FMT_RGBA_8888
-            },
+        },
         .expectStride = 1088 * 4,
         .expectSize = 1920 * 1088 * 4
     },
@@ -86,7 +86,7 @@ const AllocTestPrms GRALLOC_TEST_SETS[] = {
             .height = 1920,
             .usage = HBM_USE_MEM_DMA | HBM_USE_CPU_READ | HBM_USE_CPU_WRITE,
             .format = PIXEL_FMT_BGRA_8888
-            },
+        },
         .expectStride = 1088 * 4,
         .expectSize = 1920 * 1088 * 4
     },
@@ -241,7 +241,7 @@ const AllocTestPrms GRALLOC_TEST_SETS[] = {
             .height = 1920,
             .usage = HBM_USE_MEM_DMA | HBM_USE_CPU_READ,
             .format = PIXEL_FMT_RGBX_8888
-        }
+        },
         .expectStride = 1088 * 4,
         .expectSize = 1920 * 1088 * 4
     },
