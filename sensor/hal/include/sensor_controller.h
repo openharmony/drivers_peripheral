22a5164c8365c47086ec22651148535a96702af1
xwbmcu <xuwenbin10@huawei.com>
2023-02-02 14:40:07 +0800

    feat:add sensor DFX performance
    Signed-off-by: xwbmcu <xuwenbin10@huawei.com>

--- sensor/hal/include/sensor_controller.h
+++ sensor/hal/include/sensor_controller.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -54,5 +54,6 @@ struct SensorBasicInformation {
 
 void GetSensorDeviceMethods(struct SensorInterface *device);
 void ReleaseAllSensorInfo(void);
+int32_t *GetSensorStatus(void);
 
 #endif /* HDI_SENSOR_CONTROLLER_H */
