f9ed7123b4e3557f4239e7c09a7d6e7fe82023b1
waaaaanyang <757303947@qq.com>
2023-02-03 15:40:39 +0800

    fix: modify the corresponding code to match the config file
    Signed-off-by: waaaaanyang <757303947@qq.com>

--- battery/interfaces/hdi_service/test/unittest/src/battery_config_test.cpp
+++ battery/interfaces/hdi_service/test/unittest/src/battery_config_test.cpp
@@ -147,7 +147,7 @@ HWTEST_F (BatteryConfigTest, BatteryConfig007, TestSize.Level1)
  */
 HWTEST_F (BatteryConfigTest, BatteryConfig008, TestSize.Level1)
 {
-    std::string key = "soc.event";
+    std::string key = "soc.low";
     if (!g_configTest.IsExist(key)) {
         BATTERY_HILOGD(LABEL_TEST, "BatteryConfig008 %{public}s does not exist", key.c_str());
         return;
