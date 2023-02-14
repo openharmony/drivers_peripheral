c5d6a97c052470293d3831bf15705e38a21a75f2
wangyipeng <wangyipeng11@huawei.com>
2023-02-14 14:28:25 +0800

    fix:Modify the code compilation problem
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- usb/sample/hdi/usb_subscriber.cpp
+++ usb/sample/hdi/usb_subscriber.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,11 +13,11 @@
  * limitations under the License.
  */
 
-#include "UsbSubscriberTest.h"
-
 #include <iostream>
 #include <vector>
+#include "hdf_log.h"
 #include "osal_time.h"
+#include "UsbSubscriberTest.h"
 #include "v1_0/iusb_interface.h"
 #include "v1_0/usb_types.h"
 
@@ -26,25 +26,25 @@ using namespace OHOS::USB;
 using namespace std;
 using namespace OHOS::HDI::Usb::V1_0;
 
-using namespace {sptr<IUsbInterface> g_usbInterface = nullptr;}
-#define BIND_TIME 60
+namespace {sptr<IUsbInterface> g_usbInterface = nullptr;}
+constexpr int32_t BIND_TIME = 60;
 int32_t main(int32_t argc, const char *argv[])
 {
     g_usbInterface = IUsbInterface::Get();
     if (g_usbInterface == nullptr) {
-        printf("%s:IUsbInterface::Get() failed\n", __func__);
+        HDF_LOGE("%s:IUsbInterface::Get() failed\n", __func__);
         return HDF_FAILURE;
     }
     sptr<UsbSubscriberTest> subscriber = new UsbSubscriberTest();
-    printf("%s: bind usbd subscriber\n", __func__);
+    HDF_LOGI("%s: bind usbd subscriber\n", __func__);
     if (g_usbInterface->BindUsbdSubscriber(subscriber) != HDF_SUCCESS) {
-        printf("%s: bind usbd subscriber failed\n", __func__);
+        HDF_LOGE("%s: bind usbd subscriber failed\n", __func__);
         return HDF_FAILURE;
     }
     OsalSleep(BIND_TIME);
-    printf("%s: unbind usbd subscriber\n", __func__);
+    HDF_LOGI("%s: unbind usbd subscriber\n", __func__);
     if (g_usbInterface->UnbindUsbdSubscriber(subscriber) != HDF_SUCCESS) {
-        printf("%s: unbind usbd subscriber failed\n", __func__);
+        HDF_LOGE("%s: unbind usbd subscriber failed\n", __func__);
         return HDF_FAILURE;
     }
     return HDF_SUCCESS;
