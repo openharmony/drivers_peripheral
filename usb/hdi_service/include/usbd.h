c16b8cc693c0a75d02d5aa56f54fb60e451dbc85
openharmony_ci <120357966@qq.com>
2023-02-16 09:48:06 +0000

    回退 'Pull Request !3154 : 修改HDI头文件包含问题'

--- usb/hdi_service/include/usbd.h
+++ usb/hdi_service/include/usbd.h
@@ -21,7 +21,6 @@
 #include "hdf_device_desc.h"
 #include "osal_sem.h"
 #include "refbase.h"
-#include "iremote_object.h"
 #include "usb_ddk.h"
 #include "usb_ddk_interface.h"
 #include "usb_session.h"
