6d9ae949f4d7df3f7f51ffe0fc4eb28bdb5bb436
Liu Xingkai <liuxingkai1@huawei.com>
2023-03-24 18:18:32 +0800

    Add USB camera capability reporting function
    Signed-off-by: Liu Xingkai <liuxingkai1@huawei.com>

--- camera/hal/test/ut/v4l2/usb_camera_test.cpp
+++ camera/hal/test/ut/v4l2/usb_camera_test.cpp
@@ -62,3 +62,267 @@ TEST_F(UtestUSBCameraTest, camera_usb_0001)
         std::cout << "cameraId = " << cameraId << std::endl;
     }
 }
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_ZOOM_RATIO_RANGE
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0002)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_ZOOM_RATIO_RANGE, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        std::cout << "OHOS_ABILITY_ZOOM_RATIO_RANGE: count is " << (int)entry.count << std::endl;
+        std::cout << "Zoom ratio range: [" << entry.data.f[0];
+        std::cout << "," << entry.data.f[1] << "]" << std::endl;
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_ZOOM_RATIO_RANGE is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_CAMERA_CONNECTION_TYPE
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0003)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_CAMERA_CONNECTION_TYPE, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "OHOS_ABILITY_CAMERA_CONNECTION_TYPE value is "
+        << static_cast<int>(entry.data.u8[0]) << std::endl;
+    EXPECT_TRUE(entry.data.u8[0] == OHOS_CAMERA_CONNECTION_TYPE_USB_PLUGIN);
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_CAMERA_POSITION
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0004)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_CAMERA_POSITION, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "OHOS_ABILITY_CAMERA_POSITION value is " << static_cast<int>(entry.data.u8[0]) << std::endl;
+    EXPECT_TRUE(entry.data.u8[0] == OHOS_CAMERA_POSITION_OTHER);
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_FLASH_AVAILABLE
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0005)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_FLASH_AVAILABLE, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "OHOS_ABILITY_FLASH_AVAILABLE value is " << static_cast<int>(entry.data.u8[0]) << std::endl;
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_VIDEO_STABILIZATION_MODES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0006)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_VIDEO_STABILIZATION_MODES, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        for (int i = 0; i < entry.count; i++) {
+            std::cout << "OHOS_ABILITY_VIDEO_STABILIZATION_MODES value is "
+                    << static_cast<int>(entry.data.u8[i]) << std::endl;
+        }
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_VIDEO_STABILIZATION_MODES is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_FLASH_MODES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_007)
+{
+    ability_ = display_->GetCameraAbility();
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_FLASH_MODES, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        std::cout << "supported flash mode list:";
+        for (int i = 0; i < entry.count; i++) {
+            std::cout << " " << static_cast<int>(entry.data.u8[i]);
+        }
+        std::cout << std::endl;
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_FLASH_MODES is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_FOCUS_MODES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_008)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_FOCUS_MODES, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        std::cout << "supported focus mode list:";
+        for (int i = 0; i < entry.count; i++) {
+            std::cout << " " << static_cast<int>(entry.data.u8[i]);
+        }
+        std::cout << std::endl;
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_FOCUS_MODES is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_EXPOSURE_MODES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_009)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_EXPOSURE_MODES, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        std::cout << "supported exposure mode list:";
+        for (int i = 0; i < entry.count; i++) {
+            std::cout << " " << static_cast<int>(entry.data.u8[i]);
+        }
+        std::cout << std::endl;
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_EXPOSURE_MODES is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_CONTROL_CAPTURE_MIRROR_SUPPORTED
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0010)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_CONTROL_CAPTURE_MIRROR_SUPPORTED, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "capture mirror supported is :";
+    for (int i = 0; i < entry.count; i++) {
+        std::cout << " " << static_cast<int>(entry.data.u8[i]);
+    }
+    std::cout << std::endl;
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_MUTE_MODES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0011)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_MUTE_MODES, &entry);
+    if (ret == CAM_META_SUCCESS) {
+        std::cout << "supported mute mode is:";
+        for (int i = 0; i < entry.count; i++) {
+            std::cout << " " << static_cast<int>(entry.data.u8[i]);
+        }
+        std::cout << std::endl;
+    } else if (ret == CAM_META_ITEM_NOT_FOUND) {
+        std::cout << "OHOS_ABILITY_MUTE_MODES is not support" << std::endl;
+    }
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_FPS_RANGES
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0012)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_FPS_RANGES, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "supported fps ranges list: [";
+    std::cout << static_cast<int>(entry.data.i32[0]) << "," << static_cast<int>(entry.data.i32[1]) << "]";
+    std::cout << std::endl;
+}
+
+/**
+  * @tc.name: USB Camera
+  * @tc.desc: get value of OHOS_ABILITY_CAMERA_TYPE
+  * @tc.level: Level0
+  * @tc.size: MediumTest
+  * @tc.type: Function
+  */
+TEST_F(UtestUSBCameraTest, camera_usb_0013)
+{
+    ability_ = display_->GetCameraAbility();
+    EXPECT_NE(ability_, nullptr);
+    common_metadata_header_t *data = ability_->get();
+    camera_metadata_item_t entry;
+    int ret = FindCameraMetadataItem(data, OHOS_ABILITY_CAMERA_TYPE, &entry);
+    EXPECT_EQ(ret, CAM_META_SUCCESS);
+    std::cout << "OHOS_ABILITY_CAMERA_TYPE value is " << static_cast<int>(entry.data.u8[0]) << std::endl;
+    EXPECT_TRUE(entry.data.u8[0] == OHOS_CAMERA_TYPE_UNSPECIFIED);
+}
--- camera/hal/test/ut/v4l2/usb_camera_test.h
+++ camera/hal/test/ut/v4l2/usb_camera_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -25,5 +25,6 @@ public:
     void SetUp(void);
     void TearDown(void);
     std::shared_ptr<TestDisplay> display_ = nullptr;
+    std::shared_ptr<CameraAbility> ability_ = nullptr;
 };
 #endif
