aaeeecd53452d0d947682d1242562bd869704db9
lwx1140380 <lihuaicheng3@h-partners.com>
2023-04-13 15:55:55 +0800

    feature:Some files in the codec/hal directory are missing (C) in the copyright notice.
    Signed-off-by: lwx1140380 <lihuaicheng3@h-partners.com>

--- codec/hal/idl_service/src/component_mgr.cpp
+++ codec/hal/idl_service/src/component_mgr.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Shenzhen Kaihong DID Co., Ltd.
+ * Copyright (c) 2022-2023 Shenzhen Kaihong DID Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
