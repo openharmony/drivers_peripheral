d5c6154945477874bda3153d1eb51fc7ec9dc9eb
Zian <u202112125@hust.edu.cn>
2023-04-18 13:07:29 +0800

    fix:NULL pointer reference caused by variable hwAdapter
    Signed-off-by: Zian <u202112125@hust.edu.cn>

--- bluetooth/audio/hal/hdi_binder/proxy/src/audio_proxy_adapter.cpp
+++ bluetooth/audio/hal/hdi_binder/proxy/src/audio_proxy_adapter.cpp
@@ -499,7 +499,7 @@ int32_t AudioProxyAdapterSetPassthroughMode(struct AudioAdapter *adapter,
         return AUDIO_HAL_ERR_INTERNAL;
     }
     struct AudioHwAdapter *hwAdapter = reinterpret_cast<struct AudioHwAdapter *>(adapter);
-    if (hwAdapter == NULL || !HdfRemoteServiceWriteInterfaceToken(hwAdapter->proxyRemoteHandle, data)) {
+    if (hwAdapter == NULL || !HdfRemoteServiceWriteInterfaceToken(hwAdapter->proxyRemoteHandle, data) || hwAdapter->adapterDescriptor.adapterName == NULL) {
         AudioProxyBufReplyRecycle(data, reply);
         return AUDIO_HAL_ERR_INTERNAL;
     }
