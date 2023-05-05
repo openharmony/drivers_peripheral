ca3b38b3fd634f45fabadc327ea4728d111f29c4
luo-wei246 <luowei137@huawei.com>
2023-05-05 21:00:16 +0800

    fix:Component-based rectification
    Signed-off-by: luo-wei246 <luowei137@huawei.com>

--- usb/ddk/host/src/ddk_device_manager.c
+++ usb/ddk/host/src/ddk_device_manager.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +21,6 @@
 #include <unistd.h>
 
 #include "ddk_sysfs_device.h"
-#include "default_config.h"
 #include "hdf_base.h"
 #include "hdf_dlist.h"
 #include "hdf_io_service_if.h"
