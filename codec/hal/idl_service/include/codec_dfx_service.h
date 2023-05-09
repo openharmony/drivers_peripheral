ae8cb050cc7caec1aaaf70d6705d43563d0e3f9b
lwx1140380 <lihuaicheng3@h-partners.com>
2023-05-09 17:43:02 +0800

    feature: After the Codec IDL switch, DFX capability has been added
    Signed-off-by: lwx1140380 <lihuaicheng3@h-partners.com>

---
+++ codec/hal/idl_service/include/codec_dfx_service.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CODEC_DFX_SERVICE_H
+#define CODEC_DFX_SERVICE_H
+
+#include <map>
+#include <string>
+#include <hdf_sbuf.h>
+#include <hdf_base.h>
+#include <securec.h>
+#include "codec_component_manager_service.h"
+#include "codec_component_service.h"
+
+namespace OHOS {
+namespace HDI {
+namespace Codec {
+namespace V1_0 {
+class CodecDfxService : public RefBase {
+public:
+    ~CodecDfxService() = default;
+    static CodecDfxService &GetInstance();
+    void SetComponentManager(sptr<CodecComponentManagerService> manager);
+    static void GetBuffCount();
+    static int32_t DevCodecHostDump(struct HdfSBuf *data, struct HdfSBuf *reply);
+    static int32_t GetCodecComponentListInfo(struct HdfSBuf *reply);
+
+protected:
+    CodecDfxService() = default;
+
+private:
+    static uint32_t BuffCount_i;
+    static uint32_t BuffCount_j;
+    static uint32_t portIndex_i;
+    static uint32_t portIndex_j;
+    sptr<CodecComponentManagerService> managerService_;
+    static std::shared_ptr<OHOS::Codec::Omx::ComponentNode> dumpNode;
+    static CodecDfxService dfxInstance_;
+};
+
+}  // namespace V1_0
+}  // namespace Codec
+}  // namespace HDI
+}  // namespace OHOS
+#endif  // CODEC_DFX_SERVICE_H
