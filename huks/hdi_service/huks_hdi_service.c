8f65555ec911d6e6c11cbf75a8ba6279bab01d3d
wkyrong <wangkairong@huawei.com>
2023-05-25 19:55:59 +0800

    rewrite hdi service
    Signed-off-by: wkyrong <wangkairong@huawei.com>

--- huks/hdi_service/huks_hdi_service.c
+++ huks/hdi_service/huks_hdi_service.c
@@ -17,13 +17,15 @@
 #include <hdf_log.h>
 #include <osal_mem.h>
 #include <securec.h>
+
 #include "v1_0/ihuks.h"
 #include "v1_0/ihuks_types.h"
 
-#include "hks_core_service.h"
-#include "huks_hdi.h"
+#include "huks_hdi_passthrough_adapter.h"
+#include "huks_hdi_template.h"
+#include "huks_sa_type.h"
 
-#define HDF_LOG_TAG    huks_service
+#define HDF_LOG_TAG    huks_hdi_service
 
 struct HuksService {
     struct IHuks interface;
@@ -31,166 +33,166 @@ struct HuksService {
 
 static int32_t HuksModuleInit(struct IHuks *self)
 {
-    return HksCoreModuleInit();
+    return HuksHdiAdapterModuleInit();
 }
 
 static int32_t HuksGenerateKey(struct IHuks *self, const struct HuksBlob* keyAlias, const struct HuksParamSet* paramSet,
      const struct HuksBlob* keyIn, struct HuksBlob* encKeyOut)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_GENERATEKEY(keyAlias, paramSet, keyIn, encKeyOut, ret, HksCoreGenerateKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_GENERATEKEY(keyAlias, paramSet, keyIn, encKeyOut, ret, HuksHdiAdapterGenerateKey)
     return ret;
 }
 
 static int32_t HuksImportKey(struct IHuks *self, const struct HuksBlob* keyAlias, const struct HuksBlob* key,
      const struct HuksParamSet* paramSet, struct HuksBlob* encKeyOut)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_IMPORTKEY(keyAlias, key, paramSet, encKeyOut, ret, HksCoreImportKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_IMPORTKEY(keyAlias, key, paramSet, encKeyOut, ret, HuksHdiAdapterImportKey)
     return ret;
 }
 
 static int32_t HuksImportWrappedKey(struct IHuks *self, const struct HuksBlob* wrappingKeyAlias,
      const struct HuksBlob* wrappingEncKey, const struct HuksBlob* wrappedKeyData, const struct HuksParamSet* paramSet, struct HuksBlob* encKeyOut)
 {
-    int32_t ret = HDF_FAILURE;
+    int32_t ret = HUKS_FAILURE;
     HDI_CONVERTER_FUNC_IMPORTWRAPPEDKEY(wrappingKeyAlias, wrappingEncKey, wrappedKeyData, paramSet, encKeyOut, ret,
-        HksCoreImportWrappedKey)
+        HuksHdiAdapterImportWrappedKey)
     return ret;
 }
 
 static int32_t HuksExportPublicKey(struct IHuks *self, const struct HuksBlob* encKey,
      const struct HuksParamSet* paramSet, struct HuksBlob* keyOut)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_EXPORTPUBLICKEY(encKey, paramSet, keyOut, ret, HksCoreExportPublicKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_EXPORTPUBLICKEY(encKey, paramSet, keyOut, ret, HuksHdiAdapterExportPublicKey)
     return ret;
 }
 
 static int32_t HuksInit(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      struct HuksBlob* handle, struct HuksBlob* token)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_INIT(encKey, paramSet, handle, token, ret, HksCoreInit)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_INIT(encKey, paramSet, handle, token, ret, HuksHdiAdapterInit)
     return ret;
 }
 
 static int32_t HuksUpdate(struct IHuks *self, const struct HuksBlob* handle, const struct HuksParamSet* paramSet,
      const struct HuksBlob* inData, struct HuksBlob* outData)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_UPDATE(handle, paramSet, inData, outData, ret, HksCoreUpdate)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_UPDATE(handle, paramSet, inData, outData, ret, HuksHdiAdapterUpdate)
     return ret;
 }
 
 static int32_t HuksFinish(struct IHuks *self, const struct HuksBlob* handle, const struct HuksParamSet* paramSet,
      const struct HuksBlob* inData, struct HuksBlob* outData)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_FINISH(handle, paramSet, inData, outData, ret, HksCoreFinish)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_FINISH(handle, paramSet, inData, outData, ret, HuksHdiAdapterFinish)
     return ret;
 }
 
 static int32_t HuksAbort(struct IHuks *self, const struct HuksBlob* handle, const struct HuksParamSet* paramSet)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_ABORT(handle, paramSet, ret, HksCoreAbort)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_ABORT(handle, paramSet, ret, HuksHdiAdapterAbort)
     return ret;
 }
 
 static int32_t HuksCheckKeyValidity(struct IHuks *self, const struct HuksParamSet* paramSet,
      const struct HuksBlob* encKey)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_CHECKKEYVALIDITY(paramSet, encKey, ret, HksCoreGetKeyProperties)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_CHECKKEYVALIDITY(paramSet, encKey, ret, HuksHdiAdapterGetKeyProperties)
     return ret;
 }
 
 static int32_t HuksAttestKey(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      struct HuksBlob* certChain)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_ATTESTKEY(encKey, paramSet, certChain, ret, HksCoreAttestKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_ATTESTKEY(encKey, paramSet, certChain, ret, HuksHdiAdapterAttestKey)
     return ret;
 }
 
 static int32_t HuksGenerateRandom(struct IHuks *self, const struct HuksParamSet* paramSet, struct HuksBlob* random)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_GENERATERANDOM(paramSet, random, ret, HksCoreGenerateRandom)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_GENERATERANDOM(paramSet, random, ret, HuksHdiAdapterGenerateRandom)
     return ret;
 }
 
 static int32_t HuksSign(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      const struct HuksBlob* srcData, struct HuksBlob* signature)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_SIGN(encKey, paramSet, srcData, signature, ret, HksCoreSign)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_SIGN(encKey, paramSet, srcData, signature, ret, HuksHdiAdapterSign)
     return ret;
 }
 
 static int32_t HuksVerify(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      const struct HuksBlob* srcData, const struct HuksBlob* signature)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_VERIFY(encKey, paramSet, srcData, signature, ret, HksCoreVerify)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_VERIFY(encKey, paramSet, srcData, signature, ret, HuksHdiAdapterVerify)
     return ret;
 }
 
 static int32_t HuksEncrypt(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      const struct HuksBlob* plainText, struct HuksBlob* cipherText)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_ENCRYPT(encKey, paramSet, plainText, cipherText, ret, HksCoreEncrypt)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_ENCRYPT(encKey, paramSet, plainText, cipherText, ret, HuksHdiAdapterEncrypt)
     return ret;
 }
 
 static int32_t HuksDecrypt(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      const struct HuksBlob* cipherText, struct HuksBlob* plainText)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_DECRYPT(encKey, paramSet, cipherText, plainText, ret, HksCoreDecrypt)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_DECRYPT(encKey, paramSet, cipherText, plainText, ret, HuksHdiAdapterDecrypt)
     return ret;
 }
 
 static int32_t HuksAgreeKey(struct IHuks *self, const struct HuksParamSet* paramSet,
      const struct HuksBlob* encPrivateKey, const struct HuksBlob* peerPublicKey, struct HuksBlob* agreedKey)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_AGREEKEY(paramSet, encPrivateKey, peerPublicKey, agreedKey, ret, HksCoreAgreeKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_AGREEKEY(paramSet, encPrivateKey, peerPublicKey, agreedKey, ret, HuksHdiAdapterAgreeKey)
     return ret;
 }
 
 static int32_t HuksDeriveKey(struct IHuks *self, const struct HuksParamSet* paramSet, const struct HuksBlob* encKdfKey,
      struct HuksBlob* derivedKey)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_DERIVEKEY(paramSet, encKdfKey, derivedKey, ret, HksCoreDeriveKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_DERIVEKEY(paramSet, encKdfKey, derivedKey, ret, HuksHdiAdapterDeriveKey)
     return ret;
 }
 
 static int32_t HuksMac(struct IHuks *self, const struct HuksBlob* encKey, const struct HuksParamSet* paramSet,
      const struct HuksBlob* srcData, struct HuksBlob* mac)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_MAC(encKey, paramSet, srcData, mac, ret, HksCoreMac)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_MAC(encKey, paramSet, srcData, mac, ret, HuksHdiAdapterMac)
     return ret;
 }
 
 static int32_t HuksUpgradeKey(struct IHuks *self, const struct HuksBlob* encOldKey, const struct HuksParamSet* paramSet,
      struct HuksBlob* encNewKey)
 {
-    int32_t ret = HDF_FAILURE;
-    HDI_CONVERTER_FUNC_UPGRADEKEY(encOldKey, paramSet, encNewKey, ret, HksCoreUpgradeKey)
+    int32_t ret = HUKS_FAILURE;
+    HDI_CONVERTER_FUNC_UPGRADEKEY(encOldKey, paramSet, encNewKey, ret, HuksHdiAdapterUpgradeKey)
     return ret;
 }
 
 static int32_t HuksExportChipsetPlatformPublicKey(struct IHuks *self, const struct HuksBlob* salt,
      enum HuksChipsetPlatformDecryptScene scene, struct HuksBlob* publicKey)
 {
-    int32_t ret = HDF_FAILURE;
+    int32_t ret = HUKS_FAILURE;
     HDI_CONVERTER_FUNC_EXPORTCHIPSETPLATFORMPUBLICKEY(salt, scene, publicKey, ret,
-        HksCoreExportChipsetPlatformPublicKey)
+        HuksHdiAdapterExportChipsetPlatformPublicKey)
     return ret;
 }
 
@@ -198,7 +200,7 @@ static int32_t HuksGetVersion(struct IHuks *self, uint32_t* majorVer, uint32_t* 
 {
     *majorVer = IHUKS_MAJOR_VERSION;
     *minorVer = IHUKS_MINOR_VERSION;
-    return HDF_SUCCESS;
+    return HUKS_SUCCESS;
 }
 
 struct IHuks *HuksImplGetInstance(void)
@@ -239,5 +241,6 @@ void HuksImplRelease(struct IHuks *instance)
     if (instance == NULL) {
         return;
     }
+    (void)HuksReleaseCoreEngine();
     OsalMemFree(instance);
 }
---
+++ huks/hdi_service/huks_sa_type.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HUKS_SA_TYPE_H
+#define HUKS_SA_TYPE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * @brief HksBlob should be same with huks sa
+ */
+struct HksBlob {
+    uint32_t size;
+    uint8_t *data;
+};
+
+/**
+ * @brief HksParam should be same with huks sa
+ */
+struct HksParam {
+    uint32_t tag;
+    union {
+        bool boolParam;
+        int32_t int32Param;
+        uint32_t uint32Param;
+        uint64_t uint64Param;
+        struct HksBlob blob;
+    };
+};
+
+/**
+ * @brief HksParamSet set should be same with huks sa
+ */
+struct HksParamSet {
+    uint32_t paramSetSize;
+    uint32_t paramsCnt;
+    struct HksParam params[];
+};
+
+/**
+ * @brief hks chipset platform decrypt scene should be same with huks sa
+ */
+enum HksChipsetPlatformDecryptScene {
+    HKS_CHIPSET_PLATFORM_DECRYPT_SCENE_TA_TO_TA = 1,
+};
+
+
+/**
+ * @brief huks hdi error code should be same with huks sa
+ */
+enum HuksErrorCode {
+    HUKS_SUCCESS = 0,
+    HUKS_FAILURE = -1,
+    HUKS_ERROR_NULL_POINTER = -14,
+    HUKS_ERROR_MALLOC_FAIL = -21,
+};
+
+#endif
