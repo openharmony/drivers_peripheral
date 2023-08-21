fb105dfe35f39042bcf04887eb56e5b369653241
gao-haiyuan <gaohaiyuan1@huawei.com>
2023-08-21 02:29:39 +0000

    mock impl for user auth new hdi interface
    Signed-off-by: gao-haiyuan <gaohaiyuan1@huawei.com>

--- user_auth/hdi_service/interface/user_auth_hdi.h
+++ user_auth/hdi_service/interface/user_auth_hdi.h
@@ -45,6 +45,7 @@ using EnrolledInfo = OHOS::HDI::UserAuth::V1_0::EnrolledInfo;
 using EnrollResultInfo = OHOS::HDI::UserAuth::V1_0::EnrollResultInfo;
 
 using ScheduleInfoV1_1 = OHOS::HDI::UserAuth::V1_1::ScheduleInfoV1_1;
+using UserInfo = OHOS::HDI::UserAuth::V1_1::UserInfo;
 } // namespace UserAuth
 } // namespace HDI
 } // namespace OHOS
