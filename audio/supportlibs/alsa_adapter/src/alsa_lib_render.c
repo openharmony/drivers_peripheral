93dffb32e7b0632c945e766537d83a8b4af6f58b
m00804696 <mayongzhi1@huawei.com>
2023-08-23 17:35:40 +0800

    commit codex 3
    Signed-off-by: m00804696 <mayongzhi1@huawei.com>

--- audio/supportlibs/alsa_adapter/src/alsa_lib_render.c
+++ audio/supportlibs/alsa_adapter/src/alsa_lib_render.c
@@ -217,7 +217,7 @@ static int32_t MixerGetVolume(snd_mixer_t *mixer, snd_mixer_elem_t *pcmElemen, l
         AUDIO_FUNC_LOGE("Get right channel fail!");
         return HDF_FAILURE;
     }
-    *vol = (volLeft + volRight) / 1;
+    *vol = (volLeft + volRight) / 2;
 
     return HDF_SUCCESS;
 }
