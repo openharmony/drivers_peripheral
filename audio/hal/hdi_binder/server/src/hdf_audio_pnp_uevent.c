0611cac8afd4665cad77a86cd984ac1df5635ced
许学海 <xuxuehai2@huawei.com>
2023-10-18 06:24:28 +0000

    update audio/hal/hdi_binder/server/src/hdf_audio_pnp_uevent.c.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- audio/hal/hdi_binder/server/src/hdf_audio_pnp_uevent.c
+++ audio/hal/hdi_binder/server/src/hdf_audio_pnp_uevent.c
@@ -779,8 +779,8 @@ int32_t AudioUsbPnpUeventStartThread(void)
     ffrt_task_attr_t attr;
     ffrt_task_attr_init(&attr);
     ffrt_task_attr_set_qos(&attr, ffrt_qos_default);
-    ffrt_task_attr_set_name(&attr, threadname);
-    ffrt_submit_base(ffrt_create_function_wrapper(AudioPnpUeventStart, NULL, NULL), NULL, &attr);
+    ffrt_task_attr_set_name(&attr, threadName);
+    ffrt_submit_base(ffrt_create_function_wrapper(AudioPnpUeventStart, NULL, NULL), NULL, NULL, &attr);
 
     return HDF_SUCCESS;
 }
