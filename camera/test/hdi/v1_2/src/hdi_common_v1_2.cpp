eaf7950b5c8d5cfd6c611a95e982f5f30586fe6c
zwx1283032 <zhouyi167@huawei.com>
2023-10-24 02:02:01 +0000

    add camera hdi V1_2 test
    Signed-off-by: zwx1283032 <zhouyi167@huawei.com>

--- camera/test/hdi/v1_2/src/hdi_common_v1_2.cpp
+++ camera/test/hdi/v1_2/src/hdi_common_v1_2.cpp
@@ -97,10 +97,10 @@ void Test::Open()
         deviceCallback = new OHOS::Camera::Test::DemoCameraDeviceCallback();
 
         EXPECT_NE(serviceV1_2, nullptr);
-        rc = serviceV1_2->OpenCamera_V1_1(cameraIds.front(), deviceCallback, cameraDeviceV1_2);
+        rc = serviceV1_2->OpenCamera_V1_1(cameraIds.front(), deviceCallback, cameraDeviceV1_1);
         EXPECT_EQ(rc, HDI::Camera::V1_0::NO_ERROR);
-        EXPECT_NE(cameraDeviceV1_2, nullptr);
-        cameraDevice = static_cast<OHOS::HDI::Camera::V1_2::ICameraDevice *>(cameraDeviceV1_2.GetRefPtr());
+        EXPECT_NE(cameraDeviceV1_1, nullptr);
+        cameraDeviceV1_2 = static_cast<OHOS::HDI::Camera::V1_2::ICameraDevice *>(cameraDeviceV1_1.GetRefPtr());
         CAMERA_LOGI("OpenCamera V1_2 success");
     }
 }
