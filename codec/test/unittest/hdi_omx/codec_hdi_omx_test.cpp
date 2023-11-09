310f199f57114a95d8392d4550148d0af0309f0b
李诗高 <lee.lishigao@huawei.com>
2023-11-09 09:08:53 +0000

    update codec/test/unittest/hdi_omx/codec_hdi_omx_test.cpp.
    Signed-off-by: 李诗高 <lee.lishigao@huawei.com>

--- codec/test/unittest/hdi_omx/codec_hdi_omx_test.cpp
+++ codec/test/unittest/hdi_omx/codec_hdi_omx_test.cpp
@@ -276,7 +276,7 @@ public:
     {
         if (manager_ != nullptr && component_ != nullptr) {
             manager_->DestroyComponent(componentId_);
-            CodecComponentTypeRelease(callback_);
+            CodecComponentTypeRelease(component_);
         }
         if (callback_ != nullptr) {
             CodecCallbackTypeRelease(callback_);
