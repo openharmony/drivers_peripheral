bbd23c9c1f6512c53b22bb0d791d7c39996f3150
y00699229 <yangjiawei24@huawei.com>
2023-11-17 10:19:16 +0800

    add code about along param feature
    Signed-off-by: y00699229 <yangjiawei24@huawei.com>

--- codec/hal/idl_service/src/component_node.cpp
+++ codec/hal/idl_service/src/component_node.cpp
@@ -492,6 +492,14 @@ int32_t ComponentNode::EmptyThisBuffer(OmxCodecBuffer &buffer)
         CODEC_LOGE("EmptyOmxBuffer err [%{public}d]", err);
         return err;
     }
+    if (buffer.bufferType == CODEC_BUFFER_TYPE_DYNAMIC_HANDLE && (!codecBuffer.alongParam.empty())) {
+        CODEC_LOGE("yjw:EmptyThisBuffer along param");
+        OMXBufferAppPrivateData privateData;
+        memset(&privateData, 0, sizeof(privateData));
+        privateData.sizeOfParam = static_cast<uint32_t>(codecBuffer.alongParam.size());
+        privateData.param = static_cast<void *>(&codecBuffer.alongParam[0]);
+        omxBuffer.pAppPrivate = static_cast<void *>(&privateData);
+    }
 
     err = OMX_EmptyThisBuffer(static_cast<OMX_HANDLETYPE>(comp_), bufferHdrType);
     return err;
