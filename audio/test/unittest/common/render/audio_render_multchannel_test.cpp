8ef8202853fcfb4334e6d7ea92fa5dcdd34613fe
许学海 <xuxuehai2@huawei.com>
2023-11-25 09:30:46 +0000

    update audio/test/unittest/common/render/audio_render_multchannel_test.cpp.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- audio/test/unittest/common/render/audio_render_multchannel_test.cpp
+++ audio/test/unittest/common/render/audio_render_multchannel_test.cpp
@@ -54,15 +54,16 @@ public:
 
 void AudioUtRenderMmapTest::InitRenderAttrs(struct AudioSampleAttributes &attrs)
 {
+    attrs.format = AUDIO_FORMAT_TYPE_PCM_16_BIT;
     attrs.channelCount = AUDIO_RENDER_CHANNELCOUNT;
     attrs.sampleRate = AUDIO_SAMPLE_RATE_48K;
     attrs.interleaved = 0;
     attrs.type = AUDIO_MULTI_CHANNEL;
     attrs.period = DEEP_BUFFER_RENDER_PERIOD_SIZE;
-    attrs.frameSize = AUDIO_FORMAT_TYPE_PCM_16_BIT * AUDIO_RENDER_CHANNELCOUNT / MOVE_LEFT_NUM;
+    attrs.frameSize = AUDIO_FORMAT_TYPE_PCM_16_BIT * AUDIO_RENDER_CHANNELCOUNT;
     attrs.isBigEndian = false;
     attrs.isSignedData = true;
-    attrs.startThreshold = DEEP_BUFFER_RENDER_PERIOD_SIZE / (attrs.format * attrs.channelCount / MOVE_LEFT_NUM);
+    attrs.startThreshold = DEEP_BUFFER_RENDER_PERIOD_SIZE / (attrs.format * attrs.channelCount);
     attrs.stopThreshold = INT_MAX;
     attrs.silenceThreshold = BUFFER_LENTH;
 }
@@ -124,7 +125,6 @@ void AudioUtRenderMmapTest::SetUp()
     InitRenderDevDesc(devDescRender_);
     InitRenderAttrs(attrsRender_);
 
-    attrsRender_.format = AUDIO_FORMAT_TYPE_PCM_16_BIT;
     int32_t ret = adapter_->CreateRender(adapter_, &devDescRender_, &attrsRender_, &render_, &renderId_);
     if (ret != HDF_SUCCESS) {
         attrsRender_.format = AUDIO_FORMAT_TYPE_PCM_32_BIT;
