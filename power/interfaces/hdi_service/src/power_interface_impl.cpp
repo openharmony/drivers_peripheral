5e35dcadc604f8ba65d201f1a56ad9440a44c6fd
yangshuxiang <yangshuxiang@huawei.com>
2023-12-04 05:05:03 +0000

    fix: add json and .h
    Signed-off-by: yangshuxiang <yangshuxiang@huawei.com>

--- power/interfaces/hdi_service/src/power_interface_impl.cpp
+++ power/interfaces/hdi_service/src/power_interface_impl.cpp
@@ -40,6 +40,11 @@
 #include <thread>
 #include <unistd.h>
 
+#ifdef DRIVER_PERIPHERAL_POWER_WAKEUP_CAUSE_PATH
+#include "power_config.h"
+#endif
+
+
 namespace OHOS {
 namespace HDI {
 namespace Power {
