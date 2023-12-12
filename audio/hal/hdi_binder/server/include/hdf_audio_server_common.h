cb099ae1899ef65ef60c916a2e4b04f78a8c05ce
pxd2022 <pingxiandeng1@huawei.com>
2023-12-12 13:09:49 +0000

    update audio/hal/hdi_binder/server/include/hdf_audio_server_common.h.
    Signed-off-by: pxd2022 <pingxiandeng1@huawei.com>

--- audio/hal/hdi_binder/server/include/hdf_audio_server_common.h
+++ audio/hal/hdi_binder/server/include/hdf_audio_server_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,7 +22,6 @@
 #include "hdf_device_desc.h"
 #include "hdf_log.h"
 
-#define MAX_AUDIO_ADAPTER_NUM_SERVER    8   // Limit the number of sound cards supported to a maximum of 8
 #define STR_MAX 512
 #define CHECK_NULL_PTR_RETURN_VALUE(ptr, ret) do { \
     if ((ptr) == NULL) { \
@@ -44,64 +43,13 @@ struct HdiServiceDispatchCmdHandleList {
     AudioAllfunc func;
 };
 
-/* RenderManage Info */
-struct AudioInfoInAdapter {
-    const char *adapterName;
-    struct AudioAdapter *adapter;
-    int adapterUserNum;
-    int renderStatus;
-    int renderPriority;
-    struct AudioRender *render;
-    bool renderBusy;
-    bool renderDestory;
-    uint32_t renderPid;
-    int captureStatus;
-    int capturePriority;
-    struct AudioCapture *capture;
-    bool captureBusy;
-    bool captureDestory;
-    uint32_t capturePid;
-};
-
-int32_t AudioAdapterListGetAdapterCapture(const char *adapterName,
-    struct AudioAdapter **adapter, struct AudioCapture **capture);
-int32_t AudioDestroyCaptureInfoInAdapter(const char *adapterName);
-int32_t AudioCreatCaptureCheck(const char *adapterName, const int32_t priority);
-int32_t AudioAddCaptureInfoInAdapter(const char *adapterName,
-    struct AudioCapture *capture,
-    const struct AudioAdapter *adapter,
-    const int32_t priority,
-    uint32_t capturePid);
-int32_t AudioAdapterListGetAdapter(const char *adapterName, struct AudioAdapter **adapter);
-int32_t AudioCreatRenderCheck(const char *adapterName, const int32_t priority);
-int32_t AudioAddRenderInfoInAdapter(const char *adapterName,
-    struct AudioRender *render,
-    const struct AudioAdapter *adapter,
-    const int32_t priority,
-    uint32_t renderPid);
-int32_t AudioDestroyRenderInfoInAdapter(const char *adapterName);
-int32_t AudioAdapterListGetAdapterRender(const char *adapterName,
-    struct AudioAdapter **adapter, struct AudioRender **render);
-int32_t AudioAdapterListDestory(const char *adapterName, struct AudioAdapter **adapter);
-int32_t AudioAdapterListAdd(const char *adapterName, struct AudioAdapter *adapter);
 int32_t HdiServiceRenderCaptureReadData(struct HdfSBuf *data,
     const char **adapterName, uint32_t *pid);
-int32_t AudioAdapterListGetRender(const char *adapterName,
-    struct AudioRender **render, uint32_t pid);
-void AudioSetRenderStatus(const char *adapterName, bool renderStatus);
-int32_t AudioGetRenderStatus(const char *adapterName);
 int32_t AudioAdapterListCheckAndGetRender(struct AudioRender **render, struct HdfSBuf *data);
-int32_t AudioAdapterListGetCapture(const char *adapterName,
-    struct AudioCapture **capture, uint32_t pid);
-int32_t AudioAdapterFrameGetCapture(const char *adapterName,
-    struct AudioCapture **capture, uint32_t pid, uint32_t *index);
-void AudioSetCaptureBusy(uint32_t index, bool captureStatus);
 int32_t AudioAdapterListCheckAndGetCapture(struct AudioCapture **capture, struct HdfSBuf *data);
 int32_t ReadAudioSapmleAttrbutes(struct HdfSBuf *data, struct AudioSampleAttributes *attrs);
 int32_t WriteAudioSampleAttributes(struct HdfSBuf *reply, const struct AudioSampleAttributes *attrs);
-void AudioSetCaptureStatus(const char *adapterName, bool captureStatus);
-int32_t AudioGetCaptureStatus(const char *adapterName);
-void AdaptersServerManageInfomationRecycle(void);
+
 int32_t HdiServicePositionWrite(struct HdfSBuf *reply,
     uint64_t frames, struct AudioTimeStamp time);
 int32_t HdiServiceReqMmapBuffer(struct AudioMmapBufferDescriptor *desc, struct HdfSBuf *data);
@@ -134,4 +82,3 @@ void AudioHdiClearLoadServerFlag(void);
 int32_t HdiServiceDispatch(struct HdfDeviceIoClient *client, int cmdId, struct HdfSBuf *data,
     struct HdfSBuf *reply);
 #endif
-
