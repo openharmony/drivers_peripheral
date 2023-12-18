5cfa1def9d534cf8b23ba335573fe3a8f04fccca
梁鹏辉 <liangpenghui2@huawei.com>
2023-12-18 17:02:11 +0800

    sensor report
    Signed-off-by: l30053696 <liangpenghui2@huawei.com>

--- sensor/test/unittest/hdi/hdf_sensor_hdi_test.cpp
+++ sensor/test/unittest/hdi/hdf_sensor_hdi_test.cpp
@@ -78,7 +78,7 @@ namespace {
     constexpr int g_listNum = sizeof(g_sensorList) / sizeof(g_sensorList[0]);
     constexpr int32_t SENSOR_INTERVAL1 = 200000000;
     constexpr int32_t SENSOR_INTERVAL2 = 20000000;
-    constexpr int32_t SENSOR_INTERVAL3 = 60000000;
+    constexpr int32_t SENSOR_INTERVAL3 = 600000000;
     constexpr int32_t SENSOR_POLL_TIME = 1;
     constexpr int32_t SENSOR_WAIT_TIME = 100;
     constexpr int32_t SENSOR_WAIT_TIME2 = 20000;
