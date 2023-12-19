b384d641f4923d44d1ecff551c58abd303b6bb78
梁鹏辉 <liangpenghui2@huawei.com>
2023-12-19 15:00:54 +0800

    sensor report
    Signed-off-by: l30053696 <liangpenghui2@huawei.com>

--- sensor/hdi_impl/sensor_impl.cpp
+++ sensor/hdi_impl/sensor_impl.cpp
@@ -351,7 +351,7 @@ int32_t SensorImpl::GetSdcSensorInfo(std::vector<SdcSensorInfoVdi> &sdcSensorInf
 
     StartTrace(HITRACE_TAG_SENSORS, "GetSdcSensorInfo");
     struct SdcSensorInfo sdcSensorInfos[DEFUALT_SDC_SENSOR_INFO_SIZE];
-    int32_t ret = sensorInterface->GetSdcSensorInfo(&sdcSensorInfos);
+    int32_t ret = sensorInterface->GetSdcSensorInfo(sdcSensorInfos);
     FinishTrace(HITRACE_TAG_SENSORS);
     if (ret != SENSOR_SUCCESS) {
         HDF_LOGE("%{public}s failed, error code is %{public}d", __func__, ret);
