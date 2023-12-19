19cf68964f5bd284378804033aff5c3f934876f9
梁鹏辉 <liangpenghui2@huawei.com>
2023-12-19 14:52:32 +0800

    sensor report
    Signed-off-by: l30053696 <liangpenghui2@huawei.com>

--- sensor/hdi_impl/sensor_impl.cpp
+++ sensor/hdi_impl/sensor_impl.cpp
@@ -350,7 +350,7 @@ int32_t SensorImpl::GetSdcSensorInfo(std::vector<SdcSensorInfoVdi>& sdcSensorInf
     CHECK_SENSOR_MODULE_INSTANCE(sensorInterface, sensorInterface->GetSdcSensorInfo);
 
     StartTrace(HITRACE_TAG_SENSORS, "GetSdcSensorInfo");
-    struct SdcSensorInfo sdcSensorInfos[DEFUALT_SDC_SENSOR_INFO_SIZE];
+    struct SdcSensorInfo *sdcSensorInfos[DEFUALT_SDC_SENSOR_INFO_SIZE] = nullptr;
     int32_t ret = sensorInterface->GetSdcSensorInfo(&sdcSensorInfos);
     FinishTrace(HITRACE_TAG_SENSORS);
     if (ret != SENSOR_SUCCESS) {
