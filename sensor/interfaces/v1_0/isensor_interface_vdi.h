efb9c148c593479a2885c8cb9f5542c1cbbee88b
梁鹏辉 <liangpenghui2@huawei.com>
2023-12-25 10:56:29 +0800

    恢复修改引用
    Signed-off-by: l30053696 <liangpenghui2@huawei.com>

--- sensor/interfaces/v1_0/isensor_interface_vdi.h
+++ sensor/interfaces/v1_0/isensor_interface_vdi.h
@@ -64,8 +64,8 @@ public:
     virtual int32_t SetOption(int32_t sensorId, uint32_t option) = 0;
     virtual int32_t Register(int32_t groupId, const sptr<ISensorCallbackVdi>& callbackObj) = 0;
     virtual int32_t Unregister(int32_t groupId, const sptr<ISensorCallbackVdi>& callbackObj) = 0;
-    virtual int32_t SetSdcSensor(int32_t sensorId, bool enabled, int32_t rateLevel) { return HDF_SUCCESS; };
-    virtual int32_t GetSdcSensorInfo(std::vector<SdcSensorInfoVdi>& sdcSensorInfo) { return HDF_SUCCESS; };
+    virtual int32_t SetSdcSensor(int32_t sensorId, bool enabled, int32_t rateLevel) = 0;
+    virtual int32_t GetSdcSensorInfo(std::vector<SdcSensorInfoVdi>& sdcSensorInfo) = 0;
 };
 
 struct WrapperSensorVdi {
