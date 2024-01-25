e8d579d95402f8232bf1fa0592c305c892de2791
assertsu <subenhui@huawei.com>
2024-01-25 04:15:43 +0000

    bug fix
    Signed-off-by: assertsu <subenhui@huawei.com>

--- camera/vdi_base/usb_camera/pipeline_core/src/node/scale_node.cpp
+++ camera/vdi_base/usb_camera/pipeline_core/src/node/scale_node.cpp
@@ -97,9 +97,6 @@ void ScaleNode::PreviewScaleConver(std::shared_ptr<IBuffer>& buffer)
     buffer->SetVirAddress(virBUffer);
     buffer->SetSize(virSize);
 
-    /*avpicture_fill((AVPicture *)pFrameYUV, temp, AV_PIX_FMT_YUYV422, wide_, high_);
-    avpicture_fill((AVPicture *)pFrameRGBA, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_YUYV422,
-                   buffer->GetWidth(), buffer->GetHeight());*/
     av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, temp, AV_PIX_FMT_YUYV422, wide_, high_, 1);
     av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_YUYV422,
                    buffer->GetWidth(), buffer->GetHeight(), 1);
@@ -141,9 +138,6 @@ void ScaleNode::ScaleConver(std::shared_ptr<IBuffer>& buffer)
     pFrameRGBA = av_frame_alloc();
     uint8_t* temp = sizeVirMap.begin()->second;
 
-    /*avpicture_fill((AVPicture *)pFrameYUV, temp, AV_PIX_FMT_YUYV422, wide_, high_);
-    avpicture_fill((AVPicture *)pFrameRGBA, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_YUYV422,
-                   buffer->GetWidth(), buffer->GetHeight());*/
     av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, temp, AV_PIX_FMT_YUYV422, wide_, high_, 1);
     av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_YUYV422,
                    buffer->GetWidth(), buffer->GetHeight(), 1);
@@ -184,9 +178,6 @@ void ScaleNode::ScaleConverToYuv420(std::shared_ptr<IBuffer>& buffer)
     }
     uint8_t* temp = sizeVirMap.begin()->second;
 
-    /*avpicture_fill((AVPicture *)pFrameYUV, temp, AV_PIX_FMT_YUYV422, wide_, high_);
-    avpicture_fill((AVPicture *)pFrameRGBA, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_NV21,
-                   buffer->GetWidth(), buffer->GetHeight());*/
     av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, temp, AV_PIX_FMT_YUYV422, wide_, high_, 1);
     av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, (uint8_t *)buffer->GetVirAddress(), AV_PIX_FMT_NV21,
                    buffer->GetWidth(), buffer->GetHeight(), 1);
