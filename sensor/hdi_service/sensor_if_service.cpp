dffc77c2f75bdcfd2d87f7ebf854d72051c947a2
梁鹏辉 <liangpenghui2@huawei.com>
2024-02-01 09:23:27 +0800

    sensor sdc 双频率下发
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hdi_service/sensor_if_service.cpp
+++ sensor/hdi_service/sensor_if_service.cpp
@@ -245,8 +245,8 @@ int32_t SensorIfService::SetBatchSenior(int32_t serviceId, int32_t sensorId, int
     }
     if (mode == SDC){
         SensorClientsManager::GetInstance()->UpdateSdcSensorConfig(sensorId, sdcSamplingInterval, sdcReportInterval);
-        SensorClientsManager::GetInstance()->GetSensorBestConfig(sensorId, samplingInterval, reportInterval);
     }
+    SensorClientsManager::GetInstance()->GetSensorBestConfig(sensorId, samplingInterval, reportInterval);
     HDF_LOGD("%{public}s saSamplingInterval is %{public}d", __func__, saSamplingInterval);
     ret = sensorVdiImpl_->SetSaBatch(sensorId, saSamplingInterval, saReportInterval);
     if (ret != SENSOR_SUCCESS) {
