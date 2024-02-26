f1343fe98007365db63693026caf2d9b4de7b66c
dufresne_andy <zhaozhen30@huawei.com>
2024-02-26 17:40:35 +0800

    Description:fix usb uevent queue mechanism Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: dufresne_andy <zhaozhen30@huawei.com>

--- usb/ddk/host/src/ddk_uevent_queue.cpp
+++ usb/ddk/host/src/ddk_uevent_queue.cpp
@@ -161,9 +161,8 @@ void TaskQueue::Init(void)
             if (taskQueue_.size() > 0) {
                 DdkUeventTaskInfo task = taskQueue_.front();
                 taskQueue_.pop();
-                queueLock_.unlock();
-                conditionVariable_.notify_one();
                 DdkDispatchUevent(&task);
+                uniqueLock.unlock();
             }
         }
     };
@@ -195,6 +194,7 @@ int32_t TaskQueue::AddTask(const DdkUeventTaskInfo &task)
     std::lock_guard<std::mutex> lock(queueLock_);
     if (taskQueue_.size() > MAX_TASK_NUM) {
         HDF_LOGE("%{public}s: task queue is full", __func__);
+        conditionVariable_.notify_one();
         return HDF_FAILURE;
     }
     taskQueue_.emplace(task);
