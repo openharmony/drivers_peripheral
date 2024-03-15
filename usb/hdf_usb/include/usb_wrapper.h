1f24d9928c3eb20d6cddfe5239fbd20816a244f9
dufresne_andy <zhaozhen30@huawei.com>
2024-03-15 22:19:40 +0800

    Description:slove inconsistent parts Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: dufresne_andy <zhaozhen30@huawei.com>

---
+++ usb/hdf_usb/include/usb_wrapper.h
@@ -0,0 +1,22 @@
+ /*
+  * Copyright (c) 2023 Huawei Device Co., Ltd.
+  *
+  * HDF is dual licensed: you can use it either under the terms of
+  * the GPL, or the BSD license, at your option.
+  * See the LICENSE file in the root of this repository for complete details.
+  */
+
+#ifndef USB_WRAPPER_H
+#define USB_WRAPPER_H
+#include "hdf_base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifndef __LITEOS__
+void UsbDDKDriverMatchFailEvent(const struct UsbPnpNotifyMatchInfoTable *infoTable);
+#endif
+#ifdef __cplusplus
+};
+#endif
+#endif
