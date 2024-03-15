5a544bb7d21efb761fd3c502213e539814c9535f
xuxuehai <xuxuehai2@huawei.com>
2024-03-15 15:47:33 +0800

    commit msg
    Signed-off-by: xuxuehai <xuxuehai2@huawei.com>

--- audio/test/unittest/common/render/audio_render_multchannel_test.cpp
+++ audio/test/unittest/common/render/audio_render_multchannel_test.cpp
@@ -16,9 +16,9 @@
 #include <gtest/gtest.h>
 #include "osal_mem.h"
 
-#include "v2_0/audio_types.h"
-#include "v2_0/iaudio_manager.h"
-#include "v2_0/iaudio_render.h"
+#include "v3_0/audio_types.h"
+#include "v3_0/iaudio_manager.h"
+#include "v3_0/iaudio_render.h"
 
 using namespace std;
 using namespace testing::ext;
