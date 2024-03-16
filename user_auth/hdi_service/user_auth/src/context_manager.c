47643867c297aa1bdb80a927eee9c3bda2d50e56
openharmony_ci <120357966@qq.com>
2024-03-16 01:18:25 +0000

    回退 'Pull Request !5184 : 支持复用设备解锁结果driver'

--- user_auth/hdi_service/user_auth/src/context_manager.c
+++ user_auth/hdi_service/user_auth/src/context_manager.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Huawei Device Co., Ltd.
+ * Copyright (C) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -75,7 +75,6 @@ IAM_STATIC UserAuthContext *InitAuthContext(AuthSolutionHal params)
     context->authTrustLevel = params.authTrustLevel;
     context->collectorSensorHint = params.executorSensorHint;
     context->scheduleList = CreateLinkedList(DestroyScheduleNode);
-    context->isAuthResultCached = params.isAuthResultCached;
     if (context->scheduleList == NULL) {
         LOG_ERROR("schedule list create failed");
         Free(context);
