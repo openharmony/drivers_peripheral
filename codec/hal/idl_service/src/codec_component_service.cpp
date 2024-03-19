69b77a85ed4fcd8fa39e9d191941f58b094619d9
许学海 <xuxuehai2@huawei.com>
2024-03-19 08:28:22 +0000

    update codec/hal/idl_service/src/codec_component_service.cpp.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- codec/hal/idl_service/src/codec_component_service.cpp
+++ codec/hal/idl_service/src/codec_component_service.cpp
@@ -137,7 +137,7 @@ int32_t CodecComponentService::UseBuffer(uint32_t portIndex, const OmxCodecBuffe
 
     if (outBuffer.fd >= 0 && isIPCMode_ && outBuffer.bufferType != CODEC_BUFFER_TYPE_AVSHARE_MEM_FD &&
         outBuffer.bufferType != CODEC_BUFFER_TYPE_DMA_MEM_FD &&
-        name_.find("OMX_hisi.audio") == std::string::npos) {
+        name_.find("OMX.hisi.audio") == std::string::npos) {
         close(outBuffer.fd);
         outBuffer.fd = -1;
     }
