7b868a81ebc225fd18397c33732beb2d311e32e3
wangyikai <wangyikai14@huawei.com>
2024-03-19 11:30:29 +0800

    测试压测未打开D级别日志，修改日志级别为I级别，便于测试提供数据准确性
    Signed-off-by: wangyikai <wangyikai14@huawei.com>

--- camera/vdi_base/common/adapter/platform/v4l2/src/driver_adapter/src/v4l2_buffer.cpp
+++ camera/vdi_base/common/adapter/platform/v4l2/src/driver_adapter/src/v4l2_buffer.cpp
@@ -55,7 +55,7 @@ RetCode HosV4L2Buffers::V4L2ReqBuffers(int fd, int unsigned buffCont)
     }
 
     end = std::chrono::system_clock::now();
-    CAMERA_LOGD("ioctl(fd, VIDIOC_REQBUFS, &req), time elapse %{public}lld ms\n",
+    CAMERA_LOGI("ioctl(fd, VIDIOC_REQBUFS, &req), time elapse %{public}lld ms\n",
         std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 
     if (req.count != buffCont) {
@@ -72,7 +72,7 @@ RetCode HosV4L2Buffers::V4L2ReqBuffers(int fd, int unsigned buffCont)
             return RC_ERROR;
         }
         end = std::chrono::system_clock::now();
-        CAMERA_LOGD("ioctl(fd, VIDIOC_REQBUFS, &req), reapply memory time elapse %{public}lld ms\n",
+        CAMERA_LOGI("ioctl(fd, VIDIOC_REQBUFS, &req), reapply memory time elapse %{public}lld ms\n",
             std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 
         return RC_ERROR;
