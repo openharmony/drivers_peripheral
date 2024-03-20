74125e9c02578f1b3e85a5853ec956749e02e95c
许学海 <xuxuehai2@huawei.com>
2024-03-20 03:56:16 +0000

    update codec/hal/idl_service/src/codec_component_service.cpp.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- codec/hal/idl_service/src/codec_component_service.cpp
+++ codec/hal/idl_service/src/codec_component_service.cpp
@@ -22,7 +22,7 @@
 #include <hitrace_meter.h>
 #include "codec_log_wrapper.h"
 
-#define AUDIO_CODEC_NAME_SUBSTRING "OMX.audio"
+#define AUDIO_CODEC_NAME "OMX.audio"
 
 namespace OHOS {
 namespace HDI {
@@ -140,7 +140,7 @@ int32_t CodecComponentService::UseBuffer(uint32_t portIndex, const OmxCodecBuffe
 
     if (outBuffer.fd >= 0 && isIPCMode_ && outBuffer.bufferType != CODEC_BUFFER_TYPE_AVSHARE_MEM_FD &&
         outBuffer.bufferType != CODEC_BUFFER_TYPE_DMA_MEM_FD &&
-        name_.find(AUDIO_CODEC_NAME_SUBSTRING) == std::string::npos) {
+        name_.find(AUDIO_CODEC_NAME) == std::string::npos) {
         close(outBuffer.fd);
         outBuffer.fd = -1;
     }
