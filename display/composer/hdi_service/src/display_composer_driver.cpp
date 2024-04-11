917f08e25fd398830fcde75dd1fde53851f8159c
pxd2022 <pingxiandeng1@huawei.com>
2024-04-11 08:23:57 +0000

    update display/composer/hdi_service/src/display_composer_driver.cpp.
    Signed-off-by: pxd2022 <pingxiandeng1@huawei.com>

--- display/composer/hdi_service/src/display_composer_driver.cpp
+++ display/composer/hdi_service/src/display_composer_driver.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <pthread>
+#include <pthread.h>
 #include <hdf_base.h>
 #include <hdf_device_desc.h>
 #include <hdf_log.h>
