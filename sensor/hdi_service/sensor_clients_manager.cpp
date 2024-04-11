c54ad7703b1014e6c475296bafa6cb527421c569
梁鹏辉 <liangpenghui2@huawei.com>
2024-04-11 09:44:26 +0800

    change
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hdi_service/sensor_clients_manager.cpp
+++ sensor/hdi_service/sensor_clients_manager.cpp
@@ -186,10 +186,14 @@ void SensorClientsManager::UpdateClientPeriodCount(int sensorId, int64_t samplin
         }
         if (client.sensorConfigMap_.find(sensorId) != client.sensorConfigMap_.end()) {
             int32_t periodCount = client.sensorConfigMap_.find(sensorId)->second.samplingInterval / samplingInterval;
+            HDF_LOGD("%{public}s: serviceId=%{public}d, sensorId=%{public}d, periodCount="
+                     "%{public}" PRId64 "/%{public}" PRId64 "=%{public}d", __func__, entry.first, sensorId,
+                    samplingInterval, reportInterval, periodCount);
             if (client.periodCountMap_.find(sensorId) == client.periodCountMap_.end() ||
                 periodCount > client.periodCountMap_[sensorId]) {
                 client.periodCountMap_[sensorId] = periodCount;
-                HDF_LOGD("%{public}s: serviceId=%{public}d, sensorId=%{public}d, periodCount=%{public}" PRId64 "/%{public}" PRId64 "=%{public}d", __func__, entry.first, sensorId, samplingInterval, reportInterval, periodCount);
+            } else {
+                HDF_LOGD("%{public}s: periodCount is abandoned", __func__);
             }
         }
     }
