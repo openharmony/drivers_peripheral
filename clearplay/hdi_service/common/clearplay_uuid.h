05f122b125ede8c5a9db6c1d74a30a4e869b6dfc
baoxy92 <baoxianyue@hisilicon.com>
2024-04-29 13:43:56 +0000

    update clearplay/hdi_service/common/clearplay_uuid.h.
    Signed-off-by: baoxy92 <baoxianyue@hisilicon.com>

--- clearplay/hdi_service/common/clearplay_uuid.h
+++ clearplay/hdi_service/common/clearplay_uuid.h
@@ -23,7 +23,7 @@ namespace OHOS {
 namespace HDI {
 namespace Drm {
 namespace V1_0 {
-const std::string CLEARPLAY_UUID = "47a10ff0-ca3c-49c6-9c12-a764ffde091f";
+const std::string CLEARPLAY_UUID = "47a10ff0ca3c49c69c12a764ffde091f";
 const std::string CLEARPLAY_NAME = "com.clearplay.drm";
 
 bool IsClearPlayUuid(const std::string &uuid);
