32de75e4202de53d5033e4f41251278b48713569
luzhiye <luzhiye123@huawei.com>
2024-06-18 06:46:42 +0000

    updata std::thread for lambda
    Signed-off-by: luzhiye <luzhiye123@huawei.com>

--- camera/vdi_base/common/metadata_manager/src/metadata_controller.cpp
+++ camera/vdi_base/common/metadata_manager/src/metadata_controller.cpp
@@ -560,7 +560,7 @@ void MetadataController::Start()
     changeDataKeys_.clear();
 
     if (notifyChangedMetadata_ == nullptr) {
-        notifyChangedMetadata_ = new (std::nothrow) std::thread(&MetadataController::DealMessage, this);
+        notifyChangedMetadata_ = new (std::nothrow) std::thread([this]{ this->DealMessage(); });
         if (notifyChangedMetadata_ == nullptr) {
             CAMERA_LOGE("notifyChangedMetadata_ create failed\n");
             return;
