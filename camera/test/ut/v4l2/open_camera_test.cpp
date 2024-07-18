b28c0b1837c0df06125ea294ba5a04ade374f3a8
huangshengqing <huangshengqing8@huawei.com>
2024-07-18 08:17:04 +0000

    update camera/test/ut/v4l2/open_camera_test.cpp.
    Signed-off-by: huangshengqing <huangshengqing8@huawei.com>

--- camera/test/ut/v4l2/open_camera_test.cpp
+++ camera/test/ut/v4l2/open_camera_test.cpp
@@ -3,29 +3,29 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/license/LICENSE-2.0
- * 
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language gocerning permissions and
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 #include "open_camera_test.h"
 
-void UtestPipelineTest::SetUpTestCase(void)
+void UtestOpenCameraTest::SetUpTestCase(void)
 {}
-void UtestPipelineTest::TearDownTestCase(void)
+void UtestOpenCameraTest::TearDownTestCase(void)
 {}
-void UtestPipelineTest::SetUp(void)
+void UtestOpenCameraTest::SetUp(void)
 {
-    if(cameraBase == nullptr) {
-       cameraBase = std::make_shared<TestCameraBase>();
-       cameraBase->Init();
+    if (cameraBase == nullptr) {
+        cameraBase = std::make_shared<TestCameraBase>();
+        cameraBase->Init();
     }
 }
-void UtestPipelineTest::TearDown(void)
+void UtestOpenCameraTest::TearDown(void)
 {
     cameraBase->Close();
 }
@@ -33,47 +33,47 @@ void UtestPipelineTest::TearDown(void)
 /**
   * @tc.name: OpenCamera
   * @tc.desc: OpenCamera, success.
-  * @tc.level: level0
+  * @tc.level: Level0
   * @tc.size: MediumTest
   * @tc.type: Function
   */
 TEST_F(UtestOpenCameraTest, camera_open_0001)
 {
-    CAMERA_LOGD("OpenCamera, success.")
+    CAMERA_LOGD("OpenCamera, success.");
     std::vector<std::string> cameraIds;
     cameraBase->cameraHost->GetCameraIds(cameraIds);
     for (const auto &cameraId : cameraIds) {
-        CAMERA_LOGI("cameraId = %[public]s", cameraId.c_str());
+        CAMERA_LOGI("cameraId = %{public}s", cameraId.c_str());
     }
-    std::string cameraIds = cameraIds.front();
+    std::string cameraId = cameraIds.front();
     const OHOS::sptr<ICameraDeviceCallback> callback = new DemoCameraDeviceCallback();
     OHOS::sptr<ICameraDevice> cameraDevice;
-    cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(caneraId, callback, caneraDevice);
+    cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraDevice);
     EXPECT_EQ(true, cameraBase->rc == HDI::Camera::V1_0::NO_ERROR);
 }
 
 /**
   * @tc.name: OpenCamera cameraID input error
   * @tc.desc: OpenCamera, cameraID is not found.
-  * @tc.level: level2
+  * @tc.level: Level2
   * @tc.size: MediumTest
   * @tc.type: Function
   */
 TEST_F(UtestOpenCameraTest, camera_open_0010)
 {
-    CAMERA_LOGD("OpenCamera, cameraID is no found.");
+    CAMERA_LOGD("OpenCamera, cameraID is not found.");
     std::string cameraId = "qwerty";
     OHOS::sptr<ICameraDeviceCallback> callback = new DemoCameraDeviceCallbcak();
-    CANERA_LOGD("opencamera begin");
-    cameraBase->rc = (CameRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
+    CAMERA_LOGD("opencamera begin");
+    cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
     CAMERA_LOGD("opencamera end");
     EXPECT_EQ(INVALID_ARGUMENT, cameraBase->rc);
 }
 
 /**
   * @tc.name: OpenCamera cameraID input error
-  * @tc.desc: OpenCamera, cameraID is illegal
-  * @tc.level: level2
+  * @tc.desc: OpenCamera, cameraID is illegal.
+  * @tc.level: Level2
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -88,8 +88,8 @@ TEST_F(UtestOpenCameraTest, camera_open_0011)
 
 /**
   * @tc.name: OpenCamera cameraID input error
-  * @tc.desc: OpenCamera, cameraID is Empty
-  * @tc.level: level2
+  * @tc.desc: OpenCamera, cameraID is Empty.
+  * @tc.level: Level2
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -103,15 +103,15 @@ TEST_F(UtestOpenCameraTest, camera_open_0012)
 }
 
 /**
-  * @tc.name: OpenCamera cameraID input error
-  * @tc.desc: OpenCamera, cameraID is Null.
-  * @tc.level: level2
+  * @tc.name: OpenCamera Callback input error
+  * @tc.desc: OpenCamera, Callback is Null.
+  * @tc.level: Level2
   * @tc.size: MediumTest
   * @tc.type: Function
   */
 TEST_F(UtestOpenCameraTest, camera_open_0020)
 {
-    CAMERA_LOGD("OpenCamera, cameraID is Null.");
+    CAMERA_LOGD("OpenCamera, Callback is Null.");
     std::string cameraId = "CAMERA_FIRST";
     OHOS::sptr<ICameraDeviceCallback> callback = nullptr;
     cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
@@ -121,7 +121,7 @@ TEST_F(UtestOpenCameraTest, camera_open_0020)
 /**
   * @tc.name: OpenCamera cameraID & Callback input error
   * @tc.desc: OpenCamera, cameraID is not found, callback is null.
-  * @tc.level: level1
+  * @tc.level: Level1
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -133,13 +133,13 @@ TEST_F(UtestOpenCameraTest, camera_open_0030)
     CAMERA_LOGD("opencamera begin");
     cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
     CAMERA_LOGD("opencamera end");
-    EXPECT_EQ(INVALID_ARGUMENT, cameraBase->rc);
+    EXPECT_EQ(INVALID_ARGUMENT, cameraBase-> rc);
 }
 
 /**
   * @tc.name: OpenCamera cameraID & Callback input error
   * @tc.desc: OpenCamera, cameraID is illegal, callback is null.
-  * @tc.level: level1
+  * @tc.level: Level1
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -149,13 +149,13 @@ TEST_F(UtestOpenCameraTest, camera_open_0031)
     std::string cameraId = "1";
     OHOS::sptr<ICameraDeviceCallback> callback = nullptr;
     cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
-    EXPECT_EQ(INVALID_ARGUMENT, cameraBase->rc);
+    EXPECT_EQ(INVALID_ARGUMENT, cameraBase-> rc);
 }
 
 /**
   * @tc.name: OpenCamera cameraID & Callback input error
   * @tc.desc: OpenCamera, cameraID is Empty, callback is null.
-  * @tc.level: level1
+  * @tc.level: Level1
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -171,7 +171,7 @@ TEST_F(UtestOpenCameraTest, camera_open_0032)
 /**
   * @tc.name: Open all Cameras
   * @tc.desc: Open every Cameras what the getCameraId get.
-  * @tc.level: level2
+  * @tc.level: Level2
   * @tc.size: MediumTest
   * @tc.type: Function
   */
@@ -180,8 +180,8 @@ TEST_F(UtestOpenCameraTest, camera_open_0050)
     CAMERA_LOGD("Open all Cameras.");
     std::vector<std::string> cameraIds;
     cameraBase->cameraHost->GetCameraIds(cameraIds);
-    for (auto &cameraId :cameraIds) {
-        CAMERA_LOGI("cameraId = %{public}s", caneraId.c_str());
+    for (auto &cameraId : cameraIds) {
+        CAMERA_LOGI("cameraId = %{public}s", cameraId.c_str());
         const OHOS::sptr<ICameraDeviceCallback> callback = new DemoCameraDeviceCallback();
         cameraBase->rc = (CamRetCode)cameraBase->cameraHost->OpenCamera(cameraId, callback, cameraBase->cameraDevice);
         EXPECT_EQ(true, cameraBase->rc == HDI::Camera::V1_0::NO_ERROR);
