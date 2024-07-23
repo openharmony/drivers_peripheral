d6e184b717a59694152444248ccfc4e5feccb1f6
hechaofan <hechaofan3@huawei.com>
2024-07-23 13:31:29 +0000

    update usb/hdi_service/src/usb_impl.cpp.
    Signed-off-by: hechaofan <hechaofan3@huawei.com>

--- usb/hdi_service/src/usb_impl.cpp
+++ usb/hdi_service/src/usb_impl.cpp
@@ -1381,14 +1381,28 @@ int32_t UsbImpl::ReleaseInterface(const UsbDev &dev, uint8_t interfaceId)
         HDF_LOGE("%{public}s:ReleaseInterface failed.", __func__);
         return HDF_ERR_INVALID_PARAM;
     }
-    
+
+    struct UsbInterface *interface = nullptr;
     UsbInterfaceHandleEntity *handle = reinterpret_cast<UsbInterfaceHandleEntity *>(port->ctrDevHandle);
-    int32_t ret = RawReleaseInterface(handle->devHandle, interfaceId);
+    const UsbInterfaceHandle *interfaceHandle = (const UsbInterfaceHandle *)handle;
+    int32_t ret = GetInterfaceByHandle(interfaceHandle, &interface);
+    if (ret != HDF_SUCCESS) {
+        HDF_LOGE("%{public}s get interface failed %{public}d", __func__, ret);
+        return ret;
+    }
+
+    ret = UsbCloseInterface(interfaceHandle, false);
+    if (ret != HDF_SUCCESS) {
+        HDF_LOGE("%{public}s close interface failed %{public}d", __func__, ret);
+        return ret;
+    }
+
+    ret = UsbReleaseInterface(interface);
     if (ret != 0) {
         HDF_LOGE("%{public}s:ReleaseInterface failed, ret = %{public}d", __func__, ret);
         return HDF_ERR_INVALID_PARAM;
     }
-    return HDF_SUCCESS;
+    return ret;
 }
 
 int32_t UsbImpl::SetInterface(const UsbDev &dev, uint8_t interfaceId, uint8_t altIndex)
