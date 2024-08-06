8b95124bffaf5bfb228863bc920048f39dff7b7a
xionglei <xionglei3@huawei.com>
2024-08-06 02:27:40 +0000

    update wlan/test/unittest/chip/wifi_sta_iface_test.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wlan/test/unittest/chip/wifi_sta_iface_test.cpp
+++ wlan/test/unittest/chip/wifi_sta_iface_test.cpp
@@ -26,7 +26,6 @@ const std::string WLAN_IFNAME = "wlan0";
 const std::string AP_IFNAME = "wlan1";
 const std::string TEST_MAC = "000000";
 
-
 class WifiStaIfaceTest : public testing::Test {
 public:
     static void SetUpTestCase() {}
@@ -47,14 +46,6 @@ public:
         wifiVendorHalTest.reset();
         ifaceTool.reset();
         ifaceUtil.reset();
-        delete staIface;
-        if (staIface != nullptr) {
-            staIface = nullptr;
-        }
-        delete testIface;
-        if (testIface != nullptr) {
-            testIface = nullptr;
-        }
     }
 
 public:
