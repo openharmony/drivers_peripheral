68b88382d6bc3034061db0133bbf5b7d0aac5ed3
l60050612 <liumin222@h-partners.com>
2024-08-07 03:25:49 +0000

    update audio/hdi_service/primary_impl/vdi_src/audio_capture_vdi.c.
    Signed-off-by: l60050612 <liumin222@h-partners.com>

--- audio/hdi_service/primary_impl/vdi_src/audio_capture_vdi.c
+++ audio/hdi_service/primary_impl/vdi_src/audio_capture_vdi.c
@@ -133,9 +133,9 @@ int32_t AudioGetCapturePositionVdi(struct IAudioCapture *capture, uint64_t *fram
     CHECK_NULL_PTR_RETURN_VALUE(vdiCapture, HDF_ERR_INVALID_PARAM);
     CHECK_NULL_PTR_RETURN_VALUE(vdiCapture->GetCapturePosition, HDF_ERR_INVALID_PARAM);
 	
-HdfAudioStartTrace("Hdi:AudioGetCapturePositionVdi", 0);
+    HdfAudioStartTrace("Hdi:AudioGetCapturePositionVdi", 0);
     int32_t ret = vdiCapture->GetCapturePosition(vdiCapture, frames, (struct AudioTimeStampVdi *)time);
-HdfAudioFinishTrace();
+    HdfAudioFinishTrace();
 
     if (ret != HDF_SUCCESS) {
         AUDIO_FUNC_LOGE("audio capture get position fail, ret=%{public}d", ret);
