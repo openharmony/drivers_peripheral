d2dd539354f84e4e01ae9af0c6dd8c68d041c5e9
l60050612 <liumin222@h-partners.com>
2024-08-07 11:12:34 +0800

    audio模块新增trace维测
    Signed-off-by: l60050612 <liumin222@h-partners.com>

--- audio/hdi_service/primary_impl/vdi_src/audio_render_vdi.c
+++ audio/hdi_service/primary_impl/vdi_src/audio_render_vdi.c
@@ -112,8 +112,11 @@ int32_t AudioGetRenderPositionVdi(struct IAudioRender *render, uint64_t *frames,
     struct IAudioRenderVdi *vdiRender = renderInfo->vdiRender;
     CHECK_NULL_PTR_RETURN_VALUE(vdiRender, HDF_ERR_INVALID_PARAM);
     CHECK_NULL_PTR_RETURN_VALUE(vdiRender->GetRenderPosition, HDF_ERR_INVALID_PARAM);
-
+	
+	HdfAudioStartTrace("Hdi:AudioGetRenderPositionVdi", 0);
     int32_t ret = vdiRender->GetRenderPosition(vdiRender, frames, (struct AudioTimeStampVdi *)time);
+	HdfAudioFinishTrace();
+
     if (ret != HDF_SUCCESS) {
         AUDIO_FUNC_LOGE("audio render, get position fail, ret=%{public}d", ret);
         return ret;
