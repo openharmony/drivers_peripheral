1fe70cbb2325d0325234140bd5ee908fdce48b64
xionglei <xionglei3@huawei.com>
2024-08-09 17:05:23 +0800

    稳定性加锁
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wlan/hostapd/interfaces/hdi_service/service_common/hdi_hostapd_hal.c
+++ wlan/hostapd/interfaces/hdi_service/service_common/hdi_hostapd_hal.c
@@ -78,7 +78,7 @@ static char g_apIfaceName[IFACENAME_LEN];
 static char g_apCfgName[CFGNAME_LEN];
 #endif
 
-static pthread_mutex_t g_mutex;
+static pthread_mutex_t g_mutex =;
 
 int InitCfg(const char *ifaceName)
 {
