fc7139a10135c8d6c6f0f445686951c286209d20
秦际红 <qinjihong111@huawei.com>
2024-08-20 01:42:37 +0000

    add controlTransferReadwithLength
    Signed-off-by: 秦际红 <qinjihong111@huawei.com>

--- usb/hdi_service/src/usb_impl.cpp
+++ usb/hdi_service/src/usb_impl.cpp
@@ -1646,7 +1646,7 @@ int32_t UsbImpl::ControlTransferReadwithLength(
     controlParams.directon = (UsbRequestDirection)(((static_cast<uint32_t>(ctrlParams.requestType))
         >> DIRECTION_OFFSET_7) & ENDPOINT_DIRECTION_MASK);
     controlParams.reqType = static_cast<uint32_t>(ctrlParams.requestType);
-    controlParams.size = (ctrlParams.length <= 0 || (ctrlParams.length > MAX_CONTROL_BUFF_SIZE)
+    controlParams.size = (ctrlParams.length <= 0 || ctrlParams.length > MAX_CONTROL_BUFF_SIZE)
         ? MAX_CONTROL_BUFF_SIZE : ctrlParams.length;
     controlParams.data = static_cast<void *>(OsalMemCalloc(controlParams.size));
     if (controlParams.data == nullptr) {
