3063cf4117db9098570db82732702bb2fd053e6a
luzhiye <luzhiye123@huawei.com>
2024-08-21 08:59:47 +0000

    CI安全告警整改 资源泄漏
    Signed-off-by: luzhiye <luzhiye123@huawei.com>

--- usb/ddk/device/src/adapter_if.c
+++ usb/ddk/device/src/adapter_if.c
@@ -346,6 +346,7 @@ static int32_t UsbFnAdapterCreatInterface(const char *interfaceName, int32_t nam
     ret = snprintf_s(fnnew.name, MAX_NAMELEN, MAX_NAMELEN - 1, "%s", interfaceName);
     if (ret < 0) {
         HDF_LOGE("%{public}s: snprintf_s failed", __func__);
+        UsbFnAdapterCloseFn(fd);
         return HDF_ERR_IO;
     }
     ret = ioctl(fd, FUNCTIONFS_NEWFN, &fnnew);
