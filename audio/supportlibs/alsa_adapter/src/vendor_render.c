4fe1fb9bcc76bbf63683482768101c84223e2a4d
许学海 <xuxuehai2@huawei.com>
2024-08-28 02:53:29 +0000

    update audio/supportlibs/alsa_adapter/src/vendor_render.c.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- audio/supportlibs/alsa_adapter/src/vendor_render.c
+++ audio/supportlibs/alsa_adapter/src/vendor_render.c
@@ -58,20 +58,20 @@ static int32_t RenderSelectSceneImpl(struct AlsaRender *renderIns, enum AudioPor
 static int32_t RenderGetVolThresholdImpl(struct AlsaRender *renderIns, long *volMin, long *volMax)
 {
     int32_t ret;
-    long _volMin = 0;
-    long _volMax = 0;
+    long volMin = 0;
+    long volMax = 0;
     struct AlsaSoundCard *cardIns = (struct AlsaSoundCard *)renderIns;
     RenderData *priData = RenderGetPriData(renderIns);
     CHECK_NULL_PTR_RETURN_DEFAULT(cardIns);
     CHECK_NULL_PTR_RETURN_DEFAULT(priData);
 
-    ret = SndElementReadRange(cardIns, &priData->ctrlLeftVolume, &_volMin, &_volMax);
+    ret = SndElementReadRange(cardIns, &priData->ctrlLeftVolume, &volMin, &volMax);
     if (ret != HDF_SUCCESS) {
         AUDIO_FUNC_LOGE("SndElementReadRange fail!");
         return HDF_FAILURE;
     }
-    *volMin = _volMin;
-    *volMax = _volMax;
+    *volMin = volMin;
+    *volMax = volMax;
     
     return HDF_SUCCESS;
 }
