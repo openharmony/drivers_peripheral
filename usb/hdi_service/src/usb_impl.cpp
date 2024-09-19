0d7cf0244249e46eccecee91d6fec6df9090e47a
luzhiye <luzhiye123@huawei.com>
2024-09-19 07:33:23 +0000

    update usb/hdi_service/src/usb_impl.cpp.
    Signed-off-by: luzhiye <luzhiye123@huawei.com>

--- usb/hdi_service/src/usb_impl.cpp
+++ usb/hdi_service/src/usb_impl.cpp
@@ -1095,6 +1095,10 @@ int32_t UsbImpl::CloseDevice(const UsbDev &dev)
         HDF_LOGE("%{public}s:FindDevFromService failed", __func__);
         return HDF_DEV_ERR_NO_DEVICE;
     }
+    if (!port->initFlag) {
+        HDF_LOGE("%{public}s: openPort failed", __func__);
+        return HDF_DEV_ERR_DEV_INIT_FAIL;
+    }
     int32_t ret = 0;
     if (port->ctrDevHandle != nullptr) {
         RawUsbCloseCtlProcess(port->ctrDevHandle);
