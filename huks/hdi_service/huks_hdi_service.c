7809c22859713e05862aa40f2e480e06c257f324
zzy <zhongziyi3@huawei.com>
2024-09-27 13:01:23 +0800

    make changes for some interfaces in drivers/peripheral/huks/hdi_service/huks_hdi_services.c
    Signed-off-by: zzy <zhongziyi3@huawei.com>
    Change-Id: I9d050c769346bd96737a304e7455bdeed696457f

--- huks/hdi_service/huks_hdi_service.c
+++ huks/hdi_service/huks_hdi_service.c
@@ -211,6 +211,16 @@ static int32_t HuksUpgradeKey(struct IHuks *self, const struct HuksBlob *encOldK
     return ret;
 }
 
+static int32_t HuksExportChipsetPlatformPublicKey(struct IHuks *self, const struct HuksBlob *salt,
+    enum HuksChipsetPlatformDecryptScene scene, struct HuksBlob *publicKey)
+{
+    (void)self;
+    (void)salt;
+    (void)scene;
+    (void)publicKey;
+    return HUKS_ERROR_API_NOT_SUPPORTED;
+}
+
 static int32_t HuksGetVersion(struct IHuks *self, uint32_t *majorVer, uint32_t *minorVer)
 {
     *majorVer = IHUKS_MAJOR_VERSION;
@@ -245,6 +255,7 @@ struct IHuks *HuksImplGetInstance(void)
     service->interface.AgreeKey = HuksAgreeKey;
     service->interface.DeriveKey = HuksDeriveKey;
     service->interface.Mac = HuksMac;
+    service->interface.ExportChipsetPlatformPublicKey = HuksExportChipsetPlatformPublicKey;
     service->interface.UpgradeKey = HuksUpgradeKey;
     service->interface.GetVersion = HuksGetVersion;
     return &service->interface;
