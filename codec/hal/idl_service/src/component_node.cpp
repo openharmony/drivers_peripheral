ba996b79bac39d85cf265bce8a2877064a322619
许学海 <xuxuehai2@huawei.com>
2024-09-29 02:51:27 +0000

    update codec/hal/idl_service/src/component_node.cpp.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- codec/hal/idl_service/src/component_node.cpp
+++ codec/hal/idl_service/src/component_node.cpp
@@ -588,6 +588,9 @@ int32_t ComponentNode::UseBufferByType(uint32_t portIndex, OmxCodecBuffer &buffe
                 CHECK_AND_RETURN_RET_LOG(addr != nullptr, OMX_ErrorBadParameter, "addr is null");
                 err = OMX_UseBuffer(static_cast<OMX_HANDLETYPE>(comp_), &bufferHdrType, portIndex, 0, buffer.allocLen,
                     reinterpret_cast<uint8_t *>(addr));
+                if (::munmap(addr, static_cast<size_t>(buffer.allocLen)) != 0) {
+                    CODEC_LOGW("Error munmap");
+                }
                 break;
             }
             err = OMX_AllocateBuffer(static_cast<OMX_HANDLETYPE>(comp_), &bufferHdrType, portIndex, 0,
