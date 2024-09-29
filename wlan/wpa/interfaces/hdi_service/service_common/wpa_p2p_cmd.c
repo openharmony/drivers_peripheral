f77f99499e4546775e094ffb56f94b5cc22078f9
xionglei <xionglei3@huawei.com>
2024-09-29 15:15:21 +0800

    memcpy溢出
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wlan/wpa/interfaces/hdi_service/service_common/wpa_p2p_cmd.c
+++ wlan/wpa/interfaces/hdi_service/service_common/wpa_p2p_cmd.c
@@ -979,13 +979,13 @@ int32_t WpaInterfaceP2pConnect(struct IWpaInterface *self, const char *ifName, c
     char mode[CMD_SIZE] = {0};
     char pin[CMD_SIZE] = {0};
     char peerDevAddr[CMD_SIZE] = {0};
-    if (memcpy_s(pin, CMD_SIZE, info->pin, CMD_SIZE) != EOK) {
+    if (memcpy_s(pin, CMD_SIZE, info->pin, info->pinLen) != EOK) {
         pthread_mutex_unlock(GetInterfaceLock());
         HDF_LOGE("%{public}s strcpy failed", __func__);
         return HDF_FAILURE;
     }
 
-    if (memcpy_s(peerDevAddr, CMD_SIZE, info->peerDevAddr, CMD_SIZE) != EOK) {
+    if (memcpy_s(peerDevAddr, CMD_SIZE, info->peerDevAddr, info->peerDevAddrLen) != EOK) {
         pthread_mutex_unlock(GetInterfaceLock());
         HDF_LOGE("%{public}s strcpy failed", __func__);
         return HDF_FAILURE;
