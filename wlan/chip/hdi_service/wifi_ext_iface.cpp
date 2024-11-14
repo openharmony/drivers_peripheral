00fc9c495d168f674d32cd5364a1adf6204b0a21
xionglei <xionglei3@huawei.com>
2024-11-14 12:13:27 +0000

    update wlan/chip/hdi_service/wifi_ext_iface.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wlan/chip/hdi_service/wifi_ext_iface.cpp
+++ wlan/chip/hdi_service/wifi_ext_iface.cpp
@@ -21,7 +21,6 @@ namespace HDI {
 namespace Wlan {
 namespace Chip {
 namespace V1_0 {
-    
 WifiExtIface::WifiExtIface(
     const std::string& ifName,
     const std::weak_ptr<WifiVendorHal> vendorHal,
