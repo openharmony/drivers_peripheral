d035cc453e9f81671b048e5812e7050e86eae9dc
梁鹏辉 <liangpenghui2@huawei.com>
2024-11-14 15:04:34 +0800

    change
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- display/composer/test/unittest/hdi_composer_ut.cpp
+++ display/composer/test/unittest/hdi_composer_ut.cpp
@@ -1064,4 +1064,10 @@ HWTEST_F(DeviceTest, test_GetHDRCapabilityInfos, TestSize.Level1)
     IRect rect = {0, 0, 1920, 540};
     auto ret = g_composerDevice->SetDisplayActiveRegion(g_displayIds[0], rect);
     HDF_LOGI("SetDisplayActiveRegion ret = %{public}d", ret);
+
+    if (ret == DISPLAY_NOT_SUPPORT) {
+        DISPLAY_TEST_LOGD("SetDisplayActiveRegion not support");
+        return;
+    }
+    EXPECT_EQ(DISPLAY_SUCCESS, ret);
 }
