8460e72b3ac8b4d1d7c1c20fd5708d8b7159fd66
xieyao_whu <xieyao7@huawei.com>
2024-11-22 09:01:39 +0000

    update bluetooth/hci/hdi_service/implement/h4_protocol.h.
    Signed-off-by: xieyao_whu <xieyao7@huawei.com>

--- bluetooth/hci/hdi_service/implement/h4_protocol.h
+++ bluetooth/hci/hdi_service/implement/h4_protocol.h
@@ -17,6 +17,8 @@
 #define OHOS_HDI_BLUETOOTH_HCI_H4_PROTOCOL_H
 
 #include "hci_protocol.h"
+#include <map>
+#include <mutex>
 
 namespace OHOS {
 namespace HDI {
@@ -32,12 +34,15 @@ public:
 
 private:
     void PacketCallback();
+    void SetRTSchedule();
 
 private:
     int hciFd_ = 0;
     HciDataCallback onAclReceive_;
     HciDataCallback onScoReceive_;
     HciDataCallback onEventReceive_;
+    std::mutex tidMutex_;
+    std::map<pid_t, bool> tidMap_;
 
     uint8_t packetType_ = 0;
     std::vector<uint8_t> hciPacket_;
