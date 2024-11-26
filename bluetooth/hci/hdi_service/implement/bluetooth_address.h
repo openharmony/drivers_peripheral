840a812db45157cc17fae96877496daae39d9203
冯鹏义 <fengpengyi1@huawei.com>
2024-11-26 21:57:19 +0800

    address file
    Signed-off-by: 冯鹏义 <fengpengyi1@huawei.com>

--- bluetooth/hci/hdi_service/implement/bluetooth_address.h
+++ bluetooth/hci/hdi_service/implement/bluetooth_address.h
@@ -31,6 +31,8 @@ public:
     ~BluetoothAddress() = default;
     static std::shared_ptr<BluetoothAddress> GetDeviceAddress(const std::string &path = BT_DEVICE_ADDRESS_PATH);
     static std::shared_ptr<BluetoothAddress> GenerateDeviceAddress(const std::string &prefix = "");
+    static std::shared_ptr<BluetoothAddress> GenerateDeviceAddressFile(
+        const std::string &path = BT_DEVICE_ADDRESS_PATH);
     void ReadAddress(std::vector<uint8_t> &address) const;
     void ReadAddress(std::string &address) const;
 
@@ -38,6 +40,7 @@ private:
     static void ParseAddressToString(std::vector<uint8_t> &address, std::string &outString);
     int ParseAddressFromString(const std::string &string) const;
     static bool GetConstantAddress(char *address, int len);
+    static bool CheckAddress(char *address);
 
 private:
     std::vector<uint8_t> address_;
