3cbbbd710f852b31fe03ff26eb67b6f78ea0ce7a
Sxs <sxs233333@163.com>
2024-11-30 08:52:29 +0000

    update codec/test/unittest/idl_omx/codec_function_utils.cpp.
    Signed-off-by: Sxs <sxs233333@163.com>

--- codec/test/unittest/idl_omx/codec_function_utils.cpp
+++ codec/test/unittest/idl_omx/codec_function_utils.cpp
@@ -312,12 +312,12 @@ bool FunctionUtil::PushAlongParam(OmxCodecBuffer &omxBuffer)
         omxBuffer.alongParam.push_back(*(ptr + i));
     }
 
-    constexpr uint32_t QP_RANGE_MIN = 12;
-    constexpr uint32_t QP_RANGE_MAX = 43;
+    constexpr uint32_t qpRangeMin = 12;
+    constexpr uint32_t qpRangeMax = 43;
     CodecQPRangeParam param;
     this->InitExtParam(param);
-    param.minQp = QP_RANGE_MIN;
-    param.maxQp = QP_RANGE_MAX;
+    param.minQp = qpRangeMin;
+    param.maxQp = qpRangeMax;
     size = sizeof(param);
     ptr = reinterpret_cast<uint8_t*>(&param);
     for (uint32_t i = 0; i < size; i++) {
