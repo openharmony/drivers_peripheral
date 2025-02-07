618d4b330aaedd4887750517bf4a01f72ac3d7a6
冯鹏义 <fengpengyi1@huawei.com>
2025-02-07 06:28:33 +0000

    update bluetooth/hci/hdi_service/implement/vendor_interface.cpp.
    Signed-off-by: 冯鹏义 <fengpengyi1@huawei.com>

--- bluetooth/hci/hdi_service/implement/vendor_interface.cpp
+++ bluetooth/hci/hdi_service/implement/vendor_interface.cpp
@@ -42,7 +42,6 @@ namespace Hci {
 namespace V1_0 {
 constexpr size_t BT_VENDOR_INVALID_DATA_LEN = 0;
 constexpr int32_t RT_PRIORITY = 1;
-constexpr int32_t RS_PRIORITY = 0;
 BtVendorCallbacksT VendorInterface::vendorCallbacks_ = {
     .size = sizeof(BtVendorCallbacksT),
     .initCb = VendorInterface::OnInitCallback,
@@ -63,7 +62,6 @@ bool VendorInterface::WatchHciChannel(const ReceiveCallback &receiveCallback)
 {
     pid_t tid = gettid();
     struct sched_param setParams = {.sched_priority = RT_PRIORITY};
-    struct sched_param resetParams = {.sched_priority = RS_PRIORITY};
     int rc = sched_setscheduler(tid, SCHED_FIFO, &setParams);
     if (rc != 0) {
         HDF_LOGI("vendorInterface setscheduler failed rc:%{public}d.", rc);
