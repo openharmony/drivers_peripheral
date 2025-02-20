358fcb40887bb029f1f0bcc954cd14effa851027
l60050612 <liumin222@h-partners.com>
2025-02-20 17:12:26 +0800

    sensor追加维测
    Signed-off-by: l60050612 <liumin222@h-partners.com>

--- sensor/hdi_service/sensor_if_service.cpp
+++ sensor/hdi_service/sensor_if_service.cpp
@@ -190,7 +190,8 @@ int32_t SensorIfService::Enable(int32_t sensorId)
     uint32_t serviceId = static_cast<uint32_t>(HdfRemoteGetCallingPid());
     HDF_LOGI("%{public}s: sensorId %{public}d, serviceId = %{public}d", __func__, sensorId, serviceId);
     std::unique_lock<std::mutex> lock(sensorServiceMutex_);
-    if (!SensorClientsManager::GetInstance()->IsUpadateSensorState(sensorId, serviceId, ENABLE_SENSOR)) {
+    SensorClientsManager::GetInstance()->ReSetSensorPrintTime(sensorId);
+	if (!SensorClientsManager::GetInstance()->IsUpadateSensorState(sensorId, serviceId, ENABLE_SENSOR)) {
         return HDF_SUCCESS;
     }
 
