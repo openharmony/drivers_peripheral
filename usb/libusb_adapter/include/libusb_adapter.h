13e529aadb408eeef76a33c6f8e0682773e69438
zhaoqi <zhaoqi107@h-partners.com>
2025-02-24 14:36:31 +0800

    重构代码优化
    Signed-off-by: zhaoqi <zhaoqi107@h-partners.com>

--- usb/libusb_adapter/include/libusb_adapter.h
+++ usb/libusb_adapter/include/libusb_adapter.h
@@ -183,7 +183,7 @@ private:
     void GetCurrentDeviceList(libusb_context *ctx, sptr<V2_0::IUsbdSubscriber> subscriber);
     int32_t GetUsbDevice(const UsbDev &dev, libusb_device **device);
     int32_t FindHandleByDev(const UsbDev &dev, libusb_device_handle **handle);
-    int32_t DeleteHandleVectorAndSettingsMap(const UsbDev &dev, libusb_device_handle* handle);
+    void DeleteSettingsMap(libusb_device_handle* handle);
     int32_t DoControlTransfer(const UsbDev &dev, const UsbCtrlTransfer &ctrl, std::vector<uint8_t> &data);
     int32_t ReadDescriptors(int32_t fd, void **descriptors, size_t &descriptorsLength);
     int32_t GetUsbDevicePath(const UsbDev &dev, char *pathBuf, size_t length);
