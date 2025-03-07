683519b6b3bfd7e6e0f686227d0147f75d426deb
liushiqiang <liushiqiang4@h-partners.com>
2025-03-07 01:20:30 +0000

    update usb/test/unittest/passthrough/src/usbd_request_test.cpp. fix:solve ut fail
    Signed-off-by: liushiqiang <liushiqiang4@h-partners.com>

--- usb/test/unittest/passthrough/src/usbd_request_test.cpp
+++ usb/test/unittest/passthrough/src/usbd_request_test.cpp
@@ -1540,7 +1540,7 @@ HWTEST_F(UsbdRequestTest, UsbdRequestCancel004, TestSize.Level1)
     pipe.endpointId = CANCEL_POINTID_INVALID;
     ret = g_usbHostInterface->RequestCancel(dev, pipe);
     HDF_LOGI("UsbdRequestTest::UsbdRequestCancel004 %{public}d RequestCancel=%{public}d", __LINE__, ret);
-    EXPECT_NQ(0, ret);
+    EXPECT_NE(0, ret);
     pipe = {interfaceId, pointId};
     ret = g_usbHostInterface->RequestCancel(dev, pipe);
     HDF_LOGI("UsbdRequestTest::UsbdRequestCancel004 %{public}d again RequestCancel=%{public}d", __LINE__, ret);
