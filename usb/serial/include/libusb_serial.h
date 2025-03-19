93b37f9ab7568fe33f4252fa773a6f43c4a8e56c
dairan <dairan1@h-partners.com>
2025-03-19 09:46:54 +0000

    update usb/serial/include/libusb_serial.h.
    Signed-off-by: dairan <dairan1@h-partners.com>

--- usb/serial/include/libusb_serial.h
+++ usb/serial/include/libusb_serial.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -49,7 +49,7 @@ private:
         libusb_device_handle* handle;
         bool isOpen;
         uint8_t outputEndpointAddr;
-        uint8_t intputEndpointAddr;
+        uint8_t inputEndpointAddr;
         int32_t interface;
     } DeviceHandleInfo;
 
