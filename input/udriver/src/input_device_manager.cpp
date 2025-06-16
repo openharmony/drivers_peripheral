84c2a6f1a870e960978eec6c45326decca20a669
zhangzhongyuan <zhangzhongyuan10@huawei.com>
2025-06-16 14:39:14 +0800

    Product compatibility updater button
    Signed-off-by: zhangzhongyuan <zhangzhongyuan10@huawei.com>

--- input/udriver/src/input_device_manager.cpp
+++ input/udriver/src/input_device_manager.cpp
@@ -282,7 +282,7 @@ uint32_t GetInputDeviceTypeInfo(const string &devName)
         type = INDEV_TYPE_MOUSE;
     } else if ((devName.find("_gpio_key") != std::string::npos) ||
                (devName.find("ponkey_on") != std::string::npos) ||
-               (devName.find("gpiokey") != std::string::npos)) {
+               (devName.find("gpiokeys") != std::string::npos)) {
         type = INDEV_TYPE_KEY;
     } else if (devName.find("Touchpad") != std::string::npos) {
         type = INDEV_TYPE_TOUCHPAD;
