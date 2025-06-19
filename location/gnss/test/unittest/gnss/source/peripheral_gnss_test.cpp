17ea8d5e0cf2c8e62b10cdca6b653d4d9acd1f54
liuxinbing <liuxinbing4@h-partners.com>
2025-06-19 10:21:15 +0800

    fix tdd compile issue
    Signed-off-by: liuxinbing <liuxinbing4@h-partners.com>

--- location/gnss/test/unittest/gnss/source/peripheral_gnss_test.cpp
+++ location/gnss/test/unittest/gnss/source/peripheral_gnss_test.cpp
@@ -51,7 +51,6 @@ namespace V2_0 {
 void PeripheralGnssTest::SetUp()
 {
     gnssInstance_ = new (std::nothrow) GnssInterfaceImpl();
-    isSupportGnss = OHOS::system::GetBoolParameter(SYSPARAM_GPS_SUPPORT, false);
 }
 
 void PeripheralGnssTest::TearDown()
@@ -64,14 +63,10 @@ HWTEST_F(PeripheralGnssTest, SetGnssConfigParaTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SetGnssConfigParaTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     GnssConfigPara para;
     para.gnssBasic.gnssMode = GNSS_WORKING_MODE_MS_ASSISTED;
-    auto ret = gnssInstance_->SetGnssConfigPara(para);
+    gnssInstance_->SetGnssConfigPara(para);
     EXPECT_NE(gnssInstance_, nullptr);
 }
 
@@ -79,17 +74,13 @@ HWTEST_F(PeripheralGnssTest, NmeaCallbackTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, NmeaCallbackTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
  
-    auto ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
 
     NmeaCallback(0, nullptr, 0);
@@ -102,11 +93,7 @@ HWTEST_F(PeripheralGnssTest, GetGnssCallbackMethodsTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
        << "PeripheralGnssTest, GetGnssCallbackMethodsTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     // sub reg null brach test
     GetGnssBasicCallbackMethods(nullptr);
     GetGnssCacheCallbackMethods(nullptr);
@@ -121,16 +108,12 @@ HWTEST_F(PeripheralGnssTest, SvStatusCallbackTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SvStatusCallbackTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
-    auto ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
     SvStatusCallback(nullptr);
     GnssSatelliteStatus gnssSatelliteStatus;
@@ -145,18 +128,13 @@ HWTEST_F(PeripheralGnssTest, GnssWorkingStatusUpdateTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, GnssWorkingStatusUpdateTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
-    
+
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
 
-    auto ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
     GnssWorkingStatusUpdate(nullptr);
     uint16_t statusPtr = 1;
@@ -169,18 +147,13 @@ HWTEST_F(PeripheralGnssTest, LocationUpdateTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, LocationUpdateTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
-    
+
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
 
-    auto ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
     
     LocationUpdate(nullptr);
@@ -195,17 +168,13 @@ HWTEST_F(PeripheralGnssTest, NiNotifyCallbackTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, NiNotifyCallbackTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
 
-    auto ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
     
     NiNotifyCallback(nullptr);
@@ -219,14 +188,10 @@ HWTEST_F(PeripheralGnssTest, SendNiUserResponseTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SendNiUserResponseTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     HDF_LOGI("PeripheralGnssTest, SendNiUserResponseTest001, TestSize.Level1");
     GnssNiResponseCmd userResponse = GnssNiResponseCmd::GNSS_NI_RESPONSE_CMD_ACCEPT;
-    auto ret = gnssInstance_->SendNiUserResponse(200, userResponse);
+    gnssInstance_->SendNiUserResponse(200, userResponse);
     EXPECT_NE(gnssInstance_, nullptr);
     gnssInstance_->SendNetworkInitiatedMsg("0", 0);
     gnssInstance_->SendNetworkInitiatedMsg("0x20", 4);
@@ -235,19 +200,13 @@ HWTEST_F(PeripheralGnssTest, SendNiUserResponseTest001, TestSize.Level1)
 HWTEST_F(PeripheralGnssTest, EnableGnssTest001, TestSize.Level1)
 {
     HDF_LOGI("PeripheralGnssTest, EnableGnssTest001, TestSize.Level1");
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
 
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     sptr<IRemoteObject> saObject = samgr->CheckSystemAbility(LOCATION_LOCATOR_SA_ID);
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
 
-    int32_t ret = 0;
-    ret = gnssInstance_->EnableGnss(gnssCallback_);
+    gnssInstance_->EnableGnss(gnssCallback_);
     EXPECT_NE(gnssInstance_, nullptr);
     HDF_LOGI("PeripheralGnssTest, EnableGnssTest001, TestSize.Level1");
     gnssInstance_->DisableGnss();
@@ -257,11 +216,6 @@ HWTEST_F(PeripheralGnssTest, EnableGnssTest001, TestSize.Level1)
 HWTEST_F(PeripheralGnssTest, EnableGnssMeasurementTest001, TestSize.Level1)
 {
     HDF_LOGI("PeripheralGnssTest, EnableGnssMeasurementTest001, TestSize.Level1");
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
 
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
@@ -270,9 +224,9 @@ HWTEST_F(PeripheralGnssTest, EnableGnssMeasurementTest001, TestSize.Level1)
 
     gnssInstance_->EnableGnss(gnssCallback_);
     
-    int32_t ret = gnssInstance_->EnableGnssMeasurement(nullptr);
+    gnssInstance_->EnableGnssMeasurement(nullptr);
     gnssMeasurementCallback_ = new (std::nothrow) GnssMeasurementCallbackTest(saObject);
-    ret = gnssInstance_->EnableGnssMeasurement(gnssMeasurementCallback_);
+    gnssInstance_->EnableGnssMeasurement(gnssMeasurementCallback_);
     
     OHOS::HDI::Location::GnssMeasurementInfo gnssMeasurementInfo;
     gnssMeasurementInfo.size = 2;
@@ -289,11 +243,6 @@ HWTEST_F(PeripheralGnssTest, DisableGnssMeasurementTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, DisableGnssMeasurementTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
 
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
@@ -301,10 +250,10 @@ HWTEST_F(PeripheralGnssTest, DisableGnssMeasurementTest001, TestSize.Level1)
     auto gnssCallback_ = new (std::nothrow) GnssEventCallbackMock(saObject);
 
     gnssInstance_->EnableGnss(gnssCallback_);
-    
+
     EXPECT_NE(nullptr, gnssInstance_);
     if (gnssInstance_ != nullptr) {
-        int32_t ret = gnssInstance_->DisableGnssMeasurement();
+        gnssInstance_->DisableGnssMeasurement();
     }
     gnssInstance_->DisableGnss();
     sleep(1);
@@ -314,11 +263,7 @@ HWTEST_F(PeripheralGnssTest, SetGnssReferenceInfoTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SetGnssReferenceInfoTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     EXPECT_NE(nullptr, gnssInstance_);
     int32_t ret = 0;
 
@@ -331,23 +276,19 @@ HWTEST_F(PeripheralGnssTest, SetGnssReferenceInfoTest001, TestSize.Level1)
         refInfo.type = GnssRefInfoType::GNSS_REF_INFO_BEST_LOCATION;
         ret = gnssInstance_->SetGnssReferenceInfo(refInfo);
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, DeleteAuxiliaryData001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, DeleteAuxiliaryData001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     EXPECT_NE(nullptr, gnssInstance_);
-    int32_t ret = 0;
 
     if (gnssInstance_ != nullptr) {
         unsigned short data = 0;
-        ret = gnssInstance_->DeleteAuxiliaryData(data);
+        gnssInstance_->DeleteAuxiliaryData(data);
     }
 }
 
@@ -355,58 +296,41 @@ HWTEST_F(PeripheralGnssTest, SetPredictGnssDataTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SetPredictGnssDataTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     EXPECT_NE(nullptr, gnssInstance_);
     if (gnssInstance_ != nullptr) {
         std::string data = "test";
         int32_t ret = gnssInstance_->SetPredictGnssData(data);
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, GetCachedGnssLocationsSizeTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, GetCachedGnssLocationsSizeTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     EXPECT_NE(nullptr, gnssInstance_);
     if (gnssInstance_ != nullptr) {
         int32_t size = 0;
         int32_t ret = gnssInstance_->GetCachedGnssLocationsSize(size);
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, GetCachedGnssLocationsTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, GetCachedGnssLocationsTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     EXPECT_NE(nullptr, gnssInstance_);
     if (gnssInstance_ != nullptr) {
         int32_t ret = gnssInstance_->GetCachedGnssLocations();
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, SendNiUserResponseTest002, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SendNiUserResponseTest002, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     EXPECT_NE(nullptr, gnssInstance_);
     int32_t gnssNiNotificationId = 0;
     GnssNiResponseCmd userResponse = GnssNiResponseCmd::GNSS_NI_RESPONSE_CMD_ACCEPT;
@@ -414,49 +338,37 @@ HWTEST_F(PeripheralGnssTest, SendNiUserResponseTest002, TestSize.Level1)
         int32_t ret = gnssInstance_->SendNiUserResponse(gnssNiNotificationId, userResponse);
         EXPECT_EQ(HDF_SUCCESS, ret);
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, SendNetworkInitiatedMsg001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, SendNetworkInitiatedMsg001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     EXPECT_NE(nullptr, gnssInstance_);
     if (gnssInstance_ != nullptr) {
         std::string msg = "test";
         int length = msg.length();
         int32_t ret = gnssInstance_->SendNetworkInitiatedMsg(msg, length);
     }
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, StartGnssTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, StartGnssTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     int32_t ret = 0;
     
     ret = gnssInstance_->StartGnss(GNSS_START_TYPE_NORMAL);
     ret = gnssInstance_->StopGnss(GNSS_START_TYPE_NORMAL);
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, DoubleEnableGnssTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, DoubleEnableGnssTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
 
     sptr<ISystemAbilityManager> samgr =
         SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
@@ -468,17 +380,13 @@ HWTEST_F(PeripheralGnssTest, DoubleEnableGnssTest001, TestSize.Level1)
     auto gnssCallback_2 = new (std::nothrow) GnssEventCallbackMock(saObject);
     gnssInstance_->EnableGnss(gnssCallback_2);
     gnssInstance_->DisableGnss();
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, GetGnssMeasurementCallbackMethodsTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, GetGnssMeasurementCallbackMethodsTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
     GnssMeasurementCallbackIfaces device;
     GetGnssMeasurementCallbackMethods(nullptr);
     GetGnssMeasurementCallbackMethods(&device);
@@ -488,11 +396,7 @@ HWTEST_F(PeripheralGnssTest, GetModuleInterfaceTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, GetModuleInterfaceTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     auto locationVendorInterface = LocationVendorInterface::GetInstance();
     locationVendorInterface->GetModuleInterface(0);
     EXPECT_NE(gnssInstance_, nullptr);
@@ -502,11 +406,7 @@ HWTEST_F(PeripheralGnssTest, RestGnssTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, RestGnssTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     auto gnssImpl = new (std::nothrow) GnssInterfaceImpl();
     auto ret = gnssImpl->StartGnss(GNSS_START_TYPE_NORMAL);
     EXPECT_NE(gnssInstance_, nullptr);
@@ -516,20 +416,17 @@ HWTEST_F(PeripheralGnssTest, RestGnssTest001, TestSize.Level1)
     gnssMeasurementCallback_ = new (std::nothrow) GnssMeasurementCallbackTest(saObject);
     gnssImpl->EnableGnssMeasurement(gnssMeasurementCallback_);
     gnssImpl->ResetGnss();
+    HDF_LOGI("test ret:%{public}d", ret);
 }
 
 HWTEST_F(PeripheralGnssTest, HexCharToIntTest001, TestSize.Level1)
 {
     GTEST_LOG_(INFO)
         << "PeripheralGnssTest, HexCharToIntTest001, TestSize.Level1";
-    if (!isSupportGnss) {
-        GTEST_LOG_(INFO)
-            << "PeripheralGnssTest, not support gnss, skip tdd.";
-        return;
-    }
+
     std::string str = "1234";
     std::vector<uint8_t> ret = StringUtils::HexToByteVector(str);
-    EXPECT_NE(gnssInstance_, nullptr);
+    EXPECT_NE(ret, nullptr);
 }
 
 } // V2_0
