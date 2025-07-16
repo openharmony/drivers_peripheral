b6fa8c80dedcd636e96c4a8a79942704ca70c781
wmz <wangmingze1@hisilicon.com>
2025-07-16 11:02:35 +0000

    update codec/hal/idl_service/src/codec_dma_buffer.cpp.
    Signed-off-by: wmz <wangmingze1@hisilicon.com>

--- codec/hal/idl_service/src/codec_dma_buffer.cpp
+++ codec/hal/idl_service/src/codec_dma_buffer.cpp
@@ -26,7 +26,7 @@ namespace Codec {
 namespace Omx {
 
 sptr<ICodecBuffer> CodecDMABuffer::UseBuffer(OMX_HANDLETYPE comp, uint32_t portIndex,
-        OmxCodecBuffer &codecBuffer, OMX_BUFFERHEADERTYPE *&header)
+    OmxCodecBuffer &codecBuffer, OMX_BUFFERHEADERTYPE *&header)
 {
     CHECK_AND_RETURN_RET_LOG(comp != nullptr, nullptr, "null component");
     CHECK_AND_RETURN_RET_LOG(codecBuffer.fd != nullptr, nullptr, "invalid dma fd");
@@ -54,7 +54,7 @@ sptr<ICodecBuffer> CodecDMABuffer::UseBuffer(OMX_HANDLETYPE comp, uint32_t portI
 }
 
 sptr<ICodecBuffer> CodecDMABuffer::AllocateBuffer(OMX_HANDLETYPE comp, uint32_t portIndex,
-        OmxCodecBuffer &codecBuffer, OMX_BUFFERHEADERTYPE *&header)
+    OmxCodecBuffer &codecBuffer, OMX_BUFFERHEADERTYPE *&header)
 {
     CHECK_AND_RETURN_RET_LOG(comp != nullptr, nullptr, "null component");
     CHECK_AND_RETURN_RET_LOG(codecBuffer.allocLen > 0, nullptr, "invalid allocLen");
