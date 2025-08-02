d0a37a3b00db34ddef922b3b976aeb0cbbe6cd66
梁鹏辉 <liangpenghui2@huawei.com>
2025-08-02 16:44:39 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hdi_service_3.0/sensor_clients_manager.cpp
+++ sensor/hdi_service_3.0/sensor_clients_manager.cpp
@@ -572,7 +572,9 @@ void SensorClientsManager::HdiReportData(const sptr<V3_0::ISensorCallback> &call
         ret = callbackObj->OnDataEvent(event);
     }
     if (ret != HDF_SUCCESS) {
-        HDF_LOGD("%{public}s Sensor OnDataEvent failed, error code is %{public}d", __func__, ret);
+        HDF_LOGD("%{public}s Sensor OnDataEvent failed, error code is %{public}d, "
+            "sensorInfoId is (%{public}s,%{public}d)", __func__, ret,
+            SENSOR_INFO_ID_TO_STRING(sensorInfoId.sensorHandle).c_str(), sensorInfoId.serviceId);
     } else {
         auto it = sensorReportCountMap[sensorInfoId.sensorHandle].find(sensorInfoId.serviceId);
         int64_t reportCount = INIT_REPORT_COUNT;
