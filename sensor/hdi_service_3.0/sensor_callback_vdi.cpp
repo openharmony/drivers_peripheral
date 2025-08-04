351212a34684fdf6e1628610c3f91381f1f4585b
梁鹏辉 <liangpenghui2@huawei.com>
2025-08-04 10:20:48 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hdi_service_3.0/sensor_callback_vdi.cpp
+++ sensor/hdi_service_3.0/sensor_callback_vdi.cpp
@@ -196,7 +196,7 @@ void SensorCallbackVdi::PrintCount(const SensorHandle& sensorHandle,
         if (perSecondCount >= targetCount - acceptablError && perSecondCount <= targetCount + acceptablError) {
             return; // Skip logging if the count is within acceptable range
         }
-        HDF_LOGI("%{public}s: %{public}s perSecondCount %{public}s targetCount %{public}s~%{public}s samplingInterval "
+        HDF_LOGE("%{public}s: %{public}s perSecondCount %{public}s targetCount %{public}s~%{public}s samplingInterval "
             "%{public}s", __func__, SENSOR_HANDLE_TO_C_STR(sensorHandle), std::to_string(perSecondCount).c_str(),
             std::to_string(targetCount - acceptablError).c_str(), std::to_string(targetCount + acceptablError).c_str(),
             std::to_string(samplingInterval / ONE_MILLION).c_str());
