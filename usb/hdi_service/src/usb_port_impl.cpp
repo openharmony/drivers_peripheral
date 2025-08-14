07cb884bbd9d2ceb5ac2b662bc78ff2615b03039
Ethan Fan <hechaofan3@huawei.com>
2025-08-14 01:16:21 +0000

    update usb/hdi_service/src/usb_port_impl.cpp.
    Signed-off-by: Ethan Fan <hechaofan3@huawei.com>

--- usb/hdi_service/src/usb_port_impl.cpp
+++ usb/hdi_service/src/usb_port_impl.cpp
@@ -36,7 +36,7 @@
 #include "usbd_wrapper.h"
 
 #define HDF_LOG_TAG UsbPortImpl
-constexpr int32_t DEVICE = 2;
+constexpr int32_t DEFAULT_DEVICE_ROLE = 2;
 using namespace OHOS::HiviewDFX;
 bool g_productFlag = false;
 namespace OHOS {
@@ -106,7 +106,7 @@ int32_t UsbPortImpl::QueryPorts(std::vector<UsbPort>& portList)
             return ret;
         }
         if (!portList.empty()) {
-            portList[0].usbPortStatus.currentDataRole = DEVICE;
+            portList[0].usbPortStatus.currentDataRole = DEFAULT_DEVICE_ROLE;
         }
         return HDF_SUCCESS;
     }
