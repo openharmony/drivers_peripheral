3947e5c764acbf0d88eea5691ee9ddcf700bede4
xuxuehai <xuxuehai2@huawei.com>
2025-08-14 11:54:55 +0800

    代码同步
    Signed-off-by: xuxuehai <xuxuehai2@huawei.com>

--- usb/ddk_service/usb_service/src/usb_ddk_service.cpp
+++ usb/ddk_service/usb_service/src/usb_ddk_service.cpp
@@ -238,7 +238,7 @@ static int32_t UsbdPnpEventHandler(void *priv, uint32_t id, HdfSBuf *data)
         std::vector<uint64_t> interfaceHandleList;
         if (UsbDdkGetAllRecords({0, infoTable->busNum, infoTable->devNum}, interfaceHandleList)) {
             for (auto interfaceHandle : interfaceHandleList) {
-                HDF_LOGD("%{public}s: need release interface, handle: %{public}lu", __func__, interfaceHandle);
+                HDF_LOGD("%{public}s: need release interface, handle: %{public}llu", __func__, interfaceHandle);
                 ReleaseUsbInterface(interfaceHandle);
             }
         }
@@ -438,7 +438,7 @@ int32_t UsbDdkService::ReleaseInterface(uint64_t interfaceHandle)
         HDF_LOGE("%{public}s: no permission", __func__);
         return HDF_ERR_NOPERM;
     }
-    HDF_LOGD("%{public}s: release handle: %{public}lu", __func__, interfaceHandle);
+    HDF_LOGD("%{public}s: release handle: %{public}llu", __func__, interfaceHandle);
     return ReleaseUsbInterface(interfaceHandle);
 }
 
