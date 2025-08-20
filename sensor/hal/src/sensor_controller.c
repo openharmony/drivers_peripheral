3cdf5df5bee2dc2f20f41bca79361d75e88252f8
梁鹏辉 <liangpenghui2@huawei.com>
2025-08-20 10:31:42 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hal/src/sensor_controller.c
+++ sensor/hal/src/sensor_controller.c
@@ -436,7 +436,11 @@ static int32_t ReadData(int32_t sensorId, struct SensorEvents *event)
     struct HdfSBuf *msg = HdfSbufObtainDefaultSize();
     CHECK_NULL_PTR_RETURN_VALUE(msg, SENSOR_NULL_PTR);
     struct HdfSBuf *reply = HdfSbufObtain(HDF_SENSOR_EVENT_MAX_SIZE);
-    CHECK_NULL_PTR_RETURN_VALUE(reply, SENSOR_NULL_PTR);
+    if (reply == NULL) {
+        HDF_LOGE("%s: Sensor write id fail!", __func__);
+        HdfSbufRecycle(msg);
+        return SENSOR_FAILURE;
+    }
 
     if (!HdfSbufWriteInt32(msg, sensorId)) {
         HDF_LOGE("%{public}s: Sensor write id failed", __func__);
