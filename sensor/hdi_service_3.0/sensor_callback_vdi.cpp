db49824d1df1fcd53136e31f0524b2725106433c
梁鹏辉 <liangpenghui2@huawei.com>
2025-08-21 20:44:09 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- sensor/hdi_service_3.0/sensor_callback_vdi.cpp
+++ sensor/hdi_service_3.0/sensor_callback_vdi.cpp
@@ -202,10 +202,10 @@ void SensorCallbackVdi::PrintCount(const SensorHandle& sensorHandle,
     //Check if the current record time exceeds one second
     if (std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRecordTime).count() >= ONE_SECOND) {
         int64_t perSecondCount = currentDataCount - lastDataCount;
-        auto currentSeconds = std::chrono::duration_cast<std::chrono::seconds>(currentTime);
+        auto currentSeconds = std::chrono::time_point_cast<std::chrono::seconds>(currentTime);
 
-        int64_t c1 = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime).count();
-        int64_t c2 = std::chrono::duration_cast<std::chrono::milliseconds>(currentSeconds).count();
+        int64_t c1 = currentTime.time_since_epoch().count();
+        int64_t c2 = std::chrono::duration_cast<std::chrono::milliseconds>(currentSeconds).time_since_epoch().count();
         HDF_LOGI("%{public}s: c1 = %{public}s c2 = %{public}s", __func__, std::to_string(c1).c_str(), std::to_string(c2).c_str());
 
         lastRecordTime = currentSeconds;
