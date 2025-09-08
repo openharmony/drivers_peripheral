ace08e1e30655850822b0b004c531e61aa127826
梁鹏辉 <liangpenghui2@huawei.com>
2025-09-08 14:22:14 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- vibrator/test/benchmarktest/hdf_vibrator_benchmark_test.cpp
+++ vibrator/test/benchmarktest/hdf_vibrator_benchmark_test.cpp
@@ -385,9 +385,11 @@ BENCHMARK_F(VibratorBenchmarkTest, GetDeviceVibratorInfo)(benchmark::State &stat
 
     std::vector<HdfVibratorInfo> info;
     for (auto _ : state) {
-        int32_t startRet = g_vibratorInterface->GetDeviceVibratorInfo(g_deviceVibratorInfo, info);
 #ifdef TV_FLAG
+        int32_t startRet = g_vibratorInterface->GetDeviceVibratorInfo(g_deviceVibratorInfo, info);
         EXPECT_EQ(startRet, HDF_SUCCESS);
+#else
+        (void)g_vibratorInterface->GetDeviceVibratorInfo(g_deviceVibratorInfo, info);
 #endif
     }
 }
