09f8d4e4d2b7f16fc1ea860382e1706f75054d33
liangpenghui <liangpenghui2@huawei.com>
2025-09-26 10:31:01 +0800

    sensor单进程多callback新增fuzz测试用例
    Co-authored-by: 梁鹏辉<liangpenghui2@huawei.com>

---
+++ sensor/test/fuzztest/sensordisablewithcallbackid_fuzzer/sensordisable_fuzzer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sensordisable_fuzzer.h"
+#include "hdf_base.h"
+#include "v3_0/sensor_interface_proxy.h"
+#include "v3_1/sensor_interface_proxy.h"
+
+using namespace OHOS::HDI::Sensor::V3_0;
+using OHOS::HDI::Sensor::V3_1::GPS_CALLBACK_ID_BEGIN;
+namespace OHOS {
+    bool SensorDisableFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        sptr<OHOS::HDI::Sensor::V3_1::ISensorInterface> g_sensorInterface =
+            OHOS::HDI::Sensor::V3_1::ISensorInterface::Get();
+        if (!g_sensorInterface->EnableWithCallbackId({0, *(int32_t *)data, 0, 0}, GPS_CALLBACK_ID_BEGIN)) {
+            result = true;
+        }
+        if (!g_sensorInterface->DisableWithCallbackId({0, *(int32_t *)data, 0, 0}, GPS_CALLBACK_ID_BEGIN)) {
+            result = true;
+        }
+        return result;
+    }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+
+    if (size < sizeof(int32_t)) {
+        return 0;
+    }
+    OHOS::SensorDisableFuzzTest(data, size);
+    return 0;
+}
+
