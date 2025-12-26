e1c98de1e023b4d021490dc8fae35b089793e88d
power_wang <wangke274@h-partners.com>
2025-12-26 17:49:56 +0800

    音频设备HDI接口升级6.0
    Signed-off-by: power_wang <wangke274@h-partners.com>

--- audio/test/fuzztest/audio_hdi_fuzzer/audiomanager_fuzzer/audiomanager_fuzzer.cpp
+++ audio/test/fuzztest/audio_hdi_fuzzer/audiomanager_fuzzer/audiomanager_fuzzer.cpp
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 #include "audiomanager_fuzzer.h"
-#include "v5_0/iaudio_manager.h"
-#include "v5_0/audio_types.h"
+#include "v6_0/iaudio_manager.h"
+#include "v6_0/audio_types.h"
 #include "hdi_service_common.h"
 
 using namespace std;
