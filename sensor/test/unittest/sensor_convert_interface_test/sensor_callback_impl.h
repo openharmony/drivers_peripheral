9c9ee10072b8f14193c6bb82ce4a3155e6de94b1
w30051647 <wangyikai14@h-partners.com>
2025-12-29 11:26:12 +0800

    1 Signed-off-by: w30051647 <wangyikai14@h-partners.com>

--- sensor/test/unittest/sensor_convert_interface_test/sensor_callback_impl.h
+++ sensor/test/unittest/sensor_convert_interface_test/sensor_callback_impl.h
@@ -97,7 +97,7 @@ public:
             printf("\033[92m[       OK ] ConvertSensorDataTest SUCCESS\033[0m\n");
             if (printDataFlag) {
                 std::string st = {0};
-                DataToStr(st, event);
+                DataToStr(st, outSensorData);
                 printf("%s: %s\n", __func__, st.c_str());
                 HDF_LOGI("%{public}s: testcase %{public}s\n", __func__, st.c_str());
             }
@@ -144,6 +144,40 @@ public:
         dataStr = arrayStr;
         str = "sensorHandle: " + SENSOR_HANDLE_TO_STRING(event.deviceSensorInfo) + ", ts: " +
               std::to_string(event.timestamp / 1e9) + ", data: " + dataStr;
+
+        OsalMemFree(origin);
+        return;
+    }
+
+    void DataToStr(std::string &str, const HdfSensorData &hdfSensorData)
+    {
+        SensorHandle sensorHandle = {hdfSensorData.deviceId, hdfSensorData.sensorTypeId, hdfSensorData.sensorId,
+                                    hdfSensorData.location};
+        void *origin = OsalMemCalloc(sizeof(uint8_t) * (hdfSensorData.data.size()));
+        if (origin == nullptr) {
+            printf("%s: OsalMemCalloc failed", __func__);
+            return;
+        }
+
+        uint8_t *eventData = static_cast<uint8_t*>(origin);
+        std::copy(hdfSensorData.data.begin(), hdfSensorData.data.end(), eventData);
+        float *data = reinterpret_cast<float*>(eventData);
+        int32_t dataLen = hdfSensorData.data.size();
+        int32_t dataDimension = static_cast<int32_t>(dataLen / sizeof(float));
+        std::string dataStr = {0};
+        char arrayStr[DATA_LEN] = {0};
+
+        for (int32_t i = 0; i < dataDimension; i++) {
+            if (sprintf_s(arrayStr + strlen(arrayStr), DATA_LEN, "[%f]", data[i]) < 0) {
+                printf("%s: sprintf_s failed", __func__);
+                OsalMemFree(origin);
+                return;
+            }
+        }
+
+        dataStr = arrayStr;
+        str = "sensorHandle: " + SENSOR_HANDLE_TO_STRING(sensorHandle) + ", ts: " +
+              std::to_string(hdfSensorData.timestamp / 1e9) + ", data: " + dataStr;
 
         OsalMemFree(origin);
         return;
