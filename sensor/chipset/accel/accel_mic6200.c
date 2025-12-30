7c3af2e8c566f086191e1d3980492c3496b0f7e0
dreamyl01 <ma_yulong@hoperun.com>
2025-12-30 17:05:33 +0800

    update: 更新文件 accel_mic6200.c
    Signed-off-by: dreamyl01 <ma_yulong@hoperun.com>

--- sensor/chipset/accel/accel_mic6200.c
+++ sensor/chipset/accel/accel_mic6200.c
@@ -110,9 +110,9 @@ static int32_t ReadMic6200Data(struct SensorCfgData *cfg, struct SensorReportEve
     struct AccelData rawData = { 0, 0, 0 };
     static uint32_t readCount = 0;
     int32_t *tmp = (int32_t *)OsalMemCalloc(sizeof(int32_t) * ACCEL_AXIS_NUM);
-	if (tmp == NULL) {
+    if (tmp == NULL) {
         return HDF_ERR_MALLOC_FAIL;
-	}
+    }
 
     CHECK_NULL_PTR_RETURN_VALUE(cfg, HDF_ERR_INVALID_PARAM);
     CHECK_NULL_PTR_RETURN_VALUE(event, HDF_ERR_INVALID_PARAM);
