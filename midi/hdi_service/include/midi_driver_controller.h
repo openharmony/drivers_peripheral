3ed3d2ddaa9f9ae0693c2517ed436df2ad8cbb93
蒋志庆 <jiangzhiqing4@h-partners.com>
2026-01-17 16:53:03 +0800

    MIDI1.0
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- midi/hdi_service/include/midi_driver_controller.h
+++ midi/hdi_service/include/midi_driver_controller.h
@@ -26,6 +26,7 @@
 #include <sys/epoll.h>
 #include <thread>
 #include <vector>
+#include <map>
 #include <mutex>
 
 namespace OHOS {
@@ -54,23 +55,52 @@ struct DeviceInfo {
     std::vector<PortInfo> inputPorts;
 };
 
-struct InputThreadContext {
-    int64_t deviceId;
-    uint32_t portIndex;
-    std::atomic<bool> quit;
-    snd_rawmidi_t *rawmidi;
-    std::vector<struct pollfd> pfds;
-    sptr<IMidiCallback> dataCallback;
-    std::thread thread;
+class MidiDeviceBase {
+public:
+    MidiDeviceBase(const DeviceInfo& info) : info_(info) {}
+    virtual ~MidiDeviceBase() = default;
+
+    virtual int32_t OpenInputPort(uint32_t portId, const sptr<IMidiCallback> &callback) = 0;
+    virtual int32_t CloseInputPort(uint32_t portId) = 0;
+    virtual int32_t OpenOutputPort(uint32_t portId) = 0;
+    virtual int32_t CloseOutputPort(uint32_t portId) = 0;
+    virtual int32_t SendMidiMessages(uint32_t portId, const std::vector<MidiMessage> &messages)= 0;
+
+    int64_t GetDeviceId() const { return info_.deviceId; }
+
+protected:
+    DeviceInfo info_;
 };
 
-struct OutputContext {
-    int64_t deviceId;
-    uint32_t portIndex;
-    bool isUmp;
-    // 使用联合体或根据 isUmp 区分使用哪个指针
-    snd_rawmidi_t *rawmidi = nullptr;
-    snd_ump_t *ump = nullptr;
+class Midi1Device : public MidiDeviceBase {
+public:
+    Midi1Device(const DeviceInfo& info) : MidiDeviceBase(info) {}
+    ~Midi1Device();
+
+    int32_t OpenInputPort(uint32_t portId, const sptr<IMidiCallback> &callback) override;
+    int32_t CloseInputPort(uint32_t portId) override;
+    int32_t OpenOutputPort(uint32_t portId) override;
+    int32_t CloseOutputPort(uint32_t portId) override;
+    int32_t SendMidiMessages(uint32_t portId, const std::vector<MidiMessage> &messages) override;
+
+private: 
+    struct InputContext {
+        std::atomic<bool> quit{false};
+        snd_rawmidi_t *rawmidi = nullptr;
+        std::vector<struct pollfd> pfds;
+        sptr<IMidiCallback> dataCallback;
+        std::thread thread;
+        int eventFd = -1; // 用于唤醒 epoll
+    };
+        
+    struct OutputContext {
+        snd_rawmidi_t *rawmidi = nullptr;
+    };
+    void InputThreadLoop(std::shared_ptr<InputContext> ctx);
+
+    std::mutex mutex_;
+    std::map<uint32_t, std::shared_ptr<InputContext>> inputs_;
+    std::map<uint32_t, std::shared_ptr<OutputContext>> outputs_;
 };
 
 class EpollHandler {
@@ -79,15 +109,10 @@ class EpollHandler {
     int32_t epollFd_ = InvaildFD;
 
 public:
-    EpollHandler()
-    {
-        epollFd_ = ::epoll_create1(0);
-    }
+    EpollHandler() { epollFd_ = ::epoll_create1(0); }
+    ~EpollHandler() { finalize(); } // Ensure close on destruct
 
-    ~EpollHandler() {}
-
-    void finalize()
-    {
+    void finalize() {
         if (epollFd_ != InvaildFD) {
             ::close(epollFd_);
             epollFd_ = InvaildFD;
@@ -98,7 +123,7 @@ public:
     {
         struct epoll_event events[EventNum];
         int32_t ready = ::epoll_wait(epollFd_, events, EventNum, tmout);
-        if (callback) {
+        if (callback && ready > 0) {
             for (int32_t i = 0; i < ready; i++) {
                 if (events[i].events & EPOLLIN) {
                     callback(events[i].data.ptr, EPOLLIN);
@@ -175,17 +200,13 @@ public:
 
 private:
     void EnumerationDeviceMidi1();
-    void EnumerationDeviceMidi2();
     void CleanupDeviceInputPorts(int64_t deviceId);
     void CleanupRemovedDevices(const std::vector<DeviceInfo> &oldDeviceList);
-    std::shared_ptr<InputThreadContext> FindInputContext(int64_t deviceId, uint32_t portId);
-    std::shared_ptr<OutputContext> FindOutputContext(int64_t deviceId, uint32_t portId);
-    void InputThread(std::shared_ptr<InputThreadContext> ctx);
+    std::shared_ptr<MidiDeviceBase> GetDeviceDriver(int64_t deviceId);
     std::vector<DeviceInfo> deviceList_;
-    std::vector<int64_t> activeDevice_;
-    std::vector<std::shared_ptr<InputThreadContext>> inputCtxs;
-    std::vector<std::shared_ptr<OutputContext>> outputCtxs_;
-    std::mutex outputMutex_;
+    std::mutex deviceListMutex_;
+    std::map<int64_t, std::shared_ptr<MidiDeviceBase>> activeDrivers_;
+    std::mutex deviceMapMutex_;
 };
 } // namespace V1_0
 } // namespace Midi
