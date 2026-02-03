3ed9dfd8a17adbf8239cc66ab5ddd526feee2b95
xiebo <xiebo@superred.com.cn>
2026-02-03 11:08:55 +0800

    添加返回值和判空检查
    Signed-off-by: xiebo <xiebo@superred.com.cn>

--- audio/hdi_service/primary_impl/src/audio_adapter.c
+++ audio/hdi_service/primary_impl/src/audio_adapter.c
@@ -770,8 +770,10 @@ int32_t AudioAdapterUpdateAudioRoute(
     }
     enum AudioPortPin descPins = hwRender->renderParam.renderMode.hwInfo.deviceDescript.pins;
 
-    hwRender->renderParam.renderMode.hwInfo.deviceDescript.pins = route->sinks[0].ext.device.type;
-    AUDIO_FUNC_LOGI("AudioAdapterUpdateAudioRoute deviceType: %{public}d", route->sinks[0].ext.device.type);
+    if (route->sinks != NULL) {
+        hwRender->renderParam.renderMode.hwInfo.deviceDescript.pins = route->sinks[0].ext.device.type;
+        AUDIO_FUNC_LOGI("AudioAdapterUpdateAudioRoute deviceType: %{public}d", route->sinks[0].ext.device.type);
+    }
     if ((*pPathSelAnalysisJson)((void *)&hwRender->renderParam, RENDER_PATH_SELECT) < 0) {
         AUDIO_FUNC_LOGE("AudioAdapterUpdateAudioRoute get router fail!");
         hwRender->renderParam.renderMode.hwInfo.deviceDescript.pins = descPins;
@@ -789,6 +791,7 @@ int32_t AudioAdapterUpdateAudioRoute(
         (*pInterfaceLibModeRender)(hwRender->devCtlHandle, &hwRender->renderParam, AUDIODRV_CTL_IOCTL_UPDATE_ROUTER);
     if (ret < 0) {
         AUDIO_FUNC_LOGE("Audio RENDER_CLOSE FAIL");
+        return HDF_FAILURE;
     }
 
     return AUDIO_SUCCESS;
