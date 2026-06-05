c19225975011f5bc39eafa5f77cc4de1dc3c48c8
xu-xuehai <xuxuehai2@huawei.com>
2026-06-05 15:40:54 +0800

    update: 更新文件 adapter_if_liteos.c
    Signed-off-by: xu-xuehai <xuxuehai2@huawei.com>

--- usb/ddk/device/src/adapter_if_liteos.c
+++ usb/ddk/device/src/adapter_if_liteos.c
@@ -261,16 +261,19 @@ static void GetHeaderStr(struct UsbFnStrings ** const strings, struct UsbFunctio
 {
     uint32_t i, j;
     uint32_t langCount = 0;
+    uint32_t strCount = 0;
     uint32_t len = 0;
     for (i = 0; strings[i] != NULL; i++) {
         langCount++;
         for (j = 0; strings[i]->strings[j].s; j++) {
             len += strlen(strings[i]->strings[j].s) + sizeof(char);
         }
+        strCount = j;
         strings[i]->strCount = j;
     }
     headerStr->magic = htole32(FUNCTIONFS_STRINGS_MAGIC);
     headerStr->length = htole32(sizeof(struct UsbFunctionfsStringsHead) + langCount * sizeof(uint16_t) + len);
+    headerStr->strCount = strCount;
     headerStr->langCount = langCount;
 }
 
