21656927da3a5167b154b1be24e4915aeaf97932
chen-yu5 <417887319@qq.com>
2026-06-21 14:59:03 +0800

    修正audio_alsa的crash的问题 Signed-off-by:chen-yu5 <417887319@qq.com>

--- audio/supportlibs/alsa_adapter/src/alsa_soundcard.c
+++ audio/supportlibs/alsa_adapter/src/alsa_soundcard.c
@@ -196,7 +196,8 @@ static int32_t CfgSaveAdapterStruct(cJSON *adapter, struct AlsaAdapterCfgInfo *i
         AUDIO_FUNC_LOGE("adapter name is null!");
         return HDF_FAILURE;
     }
-    int32_t ret = memcpy_s(info->adapterName, MAX_CARD_NAME_LEN - 1, item->valuestring, MAX_CARD_NAME_LEN - 1);
+    size_t valLen = strnlen(item->valuestring, MAX_CARD_NAME_LEN - 1);
+    int32_t ret = memcpy_s(info->adapterName, MAX_CARD_NAME_LEN, item->valuestring, valLen + 1);
     if (ret != EOK) {
         AUDIO_FUNC_LOGE("memcpy_s adapterName fail!");
         return HDF_FAILURE;
@@ -214,7 +215,8 @@ static int32_t CfgSaveAdapterStruct(cJSON *adapter, struct AlsaAdapterCfgInfo *i
         AUDIO_FUNC_LOGE("cardName is null!");
         return HDF_FAILURE;
     }
-    ret = memcpy_s(info->cardName, MAX_CARD_NAME_LEN - 1, item->valuestring, MAX_CARD_NAME_LEN - 1);
+    valLen = strnlen(item->valuestring, MAX_CARD_NAME_LEN - 1);
+    ret = memcpy_s(info->cardName, MAX_CARD_NAME_LEN, item->valuestring, valLen + 1);
     if (ret != EOK) {
         AUDIO_FUNC_LOGE("memcpy_s cardName fail!");
         return HDF_FAILURE;
