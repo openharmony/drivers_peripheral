50de7acbd39d3803727a246b50ffdc050a637adf
h60106262 <huangmengcheng@h-partners.com>
2026-08-06 11:30:30 +0800

    [OpenHarmony-6.0-Release]漏洞修复：vul-1077795561442119680、vul-1077795361088606208
    Signed-off-by: h60106262 <huangmengcheng@h-partners.com>

--- usb/hdi_service/src/usb_impl.cpp
+++ usb/hdi_service/src/usb_impl.cpp
@@ -2301,15 +2301,19 @@ int32_t UsbImpl::BindUsbdSubscriber(const sptr<IUsbdSubscriber> &subscriber)
             subscribers_[i].usbPnpListener.callBack = UsbdPnpLoaderEventReceived;
             subscribers_[i].usbPnpListener.priv = &subscribers_[i];
             subscribers_[i].remote = remote;
-            subscribers_[i].deathRecipient = new UsbImpl::UsbDeathRecipient(subscriber);
-            if (subscribers_[i].deathRecipient == nullptr) {
+            auto *recipient = new UsbImpl::UsbDeathRecipient(subscriber);
+            if (recipient == nullptr) {
                 HDF_LOGE("%{public}s: new deathRecipient failed", __func__);
+                subscribers_[i].subscriber = nullptr;
                 return HDF_FAILURE;
             }
-            bool result = subscribers_[i].remote->AddDeathRecipient(
-                static_cast<UsbDeathRecipient *>(subscribers_[i].deathRecipient));
+            subscribers_[i].deathRecipient = recipient;
+            bool result = subscribers_[i].remote->AddDeathRecipient(recipient);
             if (!result) {
                 HDF_LOGE("%{public}s:AddUsbDeathRecipient failed", __func__);
+                delete recipient;
+                subscribers_[i].deathRecipient = nullptr;
+                subscribers_[i].subscriber = nullptr;
                 return HDF_FAILURE;
             }
 
